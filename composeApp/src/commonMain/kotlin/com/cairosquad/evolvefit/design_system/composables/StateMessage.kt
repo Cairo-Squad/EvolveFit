4d5db0e8bbf1186f4e97be105d70d66c07301171
Shahd-Hatem <Shahdhatim863@gmail.com>
2025-07-30 03:36:32 +0300

    make SateMessage component

---
+++ composeApp/src/commonMain/kotlin/com/cairosquad/evolvefit/design_system/composables/StateMessage.kt
@@ -0,0 +1,54 @@
+package com.cairosquad.evolvefit.design_system.composables
+
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.vector.ImageVector
+import androidx.compose.ui.text.style.TextAlign
+import androidx.compose.ui.unit.dp
+import com.cairosquad.evolvefit.design_system.theme.Theme
+
+
+@Composable
+fun StateMessage(
+    image: ImageVector,
+    title: String,
+    description: String,
+    modifier: Modifier = Modifier
+) {
+    Column(
+        modifier = modifier
+            .fillMaxSize()
+            .padding(horizontal = 16.dp),
+        horizontalAlignment = Alignment.CenterHorizontally,
+        verticalArrangement = Arrangement.Center
+    ) {
+        Image(
+            modifier = Modifier
+                .size(width = 180.dp, height = 150.dp)
+                .padding(bottom = 16.dp),
+            imageVector = image,
+            contentDescription = null
+        )
+        Text(
+            modifier = Modifier.padding(bottom = 8.dp),
+            text = title,
+            color = Theme.color.surfaces.onSurface,
+            style = Theme.textStyle.title.mediumMedium16
+        )
+        Text(
+            text = description,
+            color = Theme.color.surfaces.onSurfaceVariant,
+            style = Theme.textStyle.label.smallRegular12,
+            textAlign = TextAlign.Center
+        )
+    }
+}
+
