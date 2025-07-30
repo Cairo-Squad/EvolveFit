1eb1a21d99f499420edf0a635b941f04b27df4b1
gehadepada <gehadepada@gmail.com>
2025-07-30 19:04:31 +0300

    component: input filed component

---
+++ composeApp/src/commonMain/kotlin/com/cairosquad/evolvefit/design_system/composables/InputField.kt
@@ -0,0 +1,262 @@
+package com.cairosquad.evolvefit.design_system.composables
+
+import androidx.compose.animation.AnimatedContent
+import androidx.compose.animation.AnimatedVisibility
+import androidx.compose.animation.core.tween
+import androidx.compose.animation.scaleIn
+import androidx.compose.animation.scaleOut
+import androidx.compose.animation.togetherWith
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.background
+import androidx.compose.foundation.border
+import androidx.compose.foundation.clickable
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.foundation.text.BasicTextField
+import androidx.compose.foundation.text.KeyboardActions
+import androidx.compose.foundation.text.KeyboardOptions
+import androidx.compose.material3.Icon
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.CompositionLocalProvider
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.saveable.rememberSaveable
+import androidx.compose.runtime.setValue
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
+import androidx.compose.ui.graphics.SolidColor
+import androidx.compose.ui.platform.LocalLayoutDirection
+import androidx.compose.ui.text.TextRange
+import androidx.compose.ui.text.input.PasswordVisualTransformation
+import androidx.compose.ui.text.input.TextFieldValue
+import androidx.compose.ui.text.input.VisualTransformation
+import androidx.compose.ui.unit.LayoutDirection
+import androidx.compose.ui.unit.dp
+import com.cairosquad.evolvefit.design_system.theme.Theme
+import org.jetbrains.compose.resources.painterResource
+import evolvefit.composeapp.generated.resources.*
+import org.jetbrains.compose.resources.DrawableResource
+import org.jetbrains.compose.ui.tooling.preview.Preview
+
+
+@Composable
+fun InputField(
+    value: String,
+    onValueChange: (String) -> Unit,
+    modifier: Modifier = Modifier,
+    placeholder: String = "",
+    error: String = "",
+    isErrorMessageShown: Boolean = true,
+    isSingleLine: Boolean = true,
+    isPasswordField: Boolean = false,
+    maxCharacters: Int? = 100,
+    leadingIcon: DrawableResource? = null,
+    trailingIcon: DrawableResource? = null,
+    onTrailingIconClick: () -> Unit = {},
+) {
+    var textFieldValue by rememberSaveable(stateSaver = TextFieldValue.Saver) {
+        mutableStateOf(TextFieldValue(text = value, selection = TextRange(value.length)))
+    }
+
+    Column(
+        modifier = modifier
+    ) {
+        BasicTextField(
+            value = textFieldValue,
+            onValueChange = { newValue ->
+                val filteredValue = if (maxCharacters != null && newValue.text.length > maxCharacters) {
+                    val truncatedText = newValue.text.take(maxCharacters)
+                    newValue.copy(
+                        text = truncatedText,
+                        selection = TextRange(truncatedText.length.coerceAtMost(newValue.selection.start))
+                    )
+                } else {
+                    newValue
+                }
+
+                textFieldValue = filteredValue
+                onValueChange(filteredValue.text)
+            },
+            modifier = Modifier
+                .clip(RoundedCornerShape(8.dp))
+                .background(Theme.color.surfaces.surfaceContainer)
+                .padding(horizontal = 12.dp, vertical = 20.dp),
+            singleLine = isSingleLine,
+            textStyle = Theme.textStyle.label.smallRegular14.copy(
+                color = Theme.color.surfaces.onSurfaceContainer
+            ),
+            decorationBox = { innerTextField ->
+                Row(
+                    modifier = Modifier.fillMaxWidth(),
+                    verticalAlignment = Alignment.CenterVertically
+                ) {
+                    TextFieldIcon(
+                        leadingIcon,
+                        error = error.isNotBlank(),
+                        modifier = Modifier.padding(end = 8.dp)
+                    )
+                    Box(
+                        modifier = Modifier.weight(1f)
+                    ) {
+                        innerTextField()
+
+                        if (textFieldValue.text.isEmpty()) {
+                            Text(
+                                text = placeholder,
+                                style = Theme.textStyle.label.smallRegular14.copy(
+                                    color = Theme.color.surfaces.onSurfaceVariant
+                                )
+                            )
+                        }
+                    }
+                    TextFieldIcon(
+                        trailingIcon,
+                        modifier = Modifier.padding(start = 8.dp),
+                        error = error.isNotBlank(),
+                        onTrailingIconClick,
+                    )
+                }
+            },
+            cursorBrush = SolidColor(
+                Theme.color.surfaces.onSurface
+            ),
+            visualTransformation = if (isPasswordField) PasswordVisualTransformation() else VisualTransformation.None,
+        )
+
+    }
+}
+@Composable
+private fun TextFieldIcon(
+    icon: DrawableResource? = null,
+    modifier: Modifier = Modifier,
+    error: Boolean = false,
+    onClick: (() -> Unit)? = null
+) {
+    val tintColor = if (error) {
+        Theme.color.system.warning
+    } else {
+         Theme.color.surfaces.onSurfaceVariant
+    }
+
+    if (icon != null) {
+        AnimatedContent(
+            targetState = icon,
+            transitionSpec = {
+                scaleIn(animationSpec = tween(300)).togetherWith(scaleOut(animationSpec = tween(300)))
+            }
+        ) {
+            Icon(
+                painter = painterResource(it),
+                contentDescription = null,
+                tint=tintColor,
+                modifier = modifier
+                    .size(20.dp)
+                    .then(
+                        if (onClick != null) {
+                            Modifier.clickable(onClick = onClick)
+                        } else {
+                            Modifier
+                        }
+                    )
+                    
+            )
+        }
+    }
+}
+
+@Composable
+@Preview
+private fun PreviewInputFieldNormal() {
+    InputField(
+        value = "",
+        onValueChange = {},
+        placeholder = "Enter your name",
+        leadingIcon = Res.drawable.ic_profile,
+        modifier = Modifier.padding(16.dp)
+    )
+}
+
+
+@Composable
+@Preview
+private fun PreviewDateInputField() {
+    InputField(
+        value = "25/2/2003",
+        onValueChange = {},
+        leadingIcon = Res.drawable.ic_date,
+        modifier = Modifier.padding(16.dp)
+    )
+}
+
+@Composable
+@Preview
+private fun PreviewInputFieldSuccess() {
+    InputField(
+        value = "Gehad",
+        onValueChange = {},
+        placeholder = "Enter your name",
+        trailingIcon = Res.drawable.trueIcon, 
+        modifier = Modifier.padding(16.dp)
+    )
+}
+
+@Composable
+@Preview
+private fun PreviewPasswordInputField() {
+    InputField(
+        value = "password123",
+        onValueChange = {},
+        placeholder = "Password",
+        isPasswordField = true,
+        leadingIcon = Res.drawable.ic_arrow_down,
+        trailingIcon = Res.drawable.ic_arrow_down,
+        onTrailingIconClick = {  },
+        modifier = Modifier.padding(16.dp)
+    )
+}
+@Composable
+@Preview
+private fun PreviewDropdownInputField() {
+    InputField(
+        value = "Option 1",
+        onValueChange = {},
+        placeholder = "Select option",
+        trailingIcon = Res.drawable.ic_arrow_down,
+        modifier = Modifier.padding(16.dp)
+    )
+}
+@Composable
+@Preview
+private fun PreviewMultilineInputField() {
+    InputField(
+        value = "This is a longer text that spans multiple lines to show how the input field behaves with multi-line content.",
+        onValueChange = {},
+        isSingleLine = false,
+        maxCharacters = 200,
+        modifier = Modifier
+            .padding(16.dp)
+            .height(100.dp)
+    )
+}
+@Composable
+@Preview
+private fun PreviewInputFieldArabicTyping() {
+    CompositionLocalProvider(LocalLayoutDirection provides LayoutDirection.Rtl) {
+        InputField(
+            value = "محمد",
+            onValueChange = {},
+            placeholder = "أدخل اسمك",
+            leadingIcon = Res.drawable.ic_profile,
+            modifier = Modifier.padding(16.dp)
+        )
+    }
+}
