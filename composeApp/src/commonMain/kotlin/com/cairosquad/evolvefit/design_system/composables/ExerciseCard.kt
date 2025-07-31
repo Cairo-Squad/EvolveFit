80e7d5a8bcdf81c9eedbf56b786b5657e856d188
NadeenXo <nadeenserag@gmail.com>
2025-07-31 14:51:25 +0300

    refactor: edit some properties

--- composeApp/src/commonMain/kotlin/com/cairosquad/evolvefit/design_system/composables/ExerciseCard.kt
+++ composeApp/src/commonMain/kotlin/com/cairosquad/evolvefit/design_system/composables/ExerciseCard.kt
@@ -1,7 +1,6 @@
 package com.cairosquad.evolvefit.design_system.composables
 
 import androidx.compose.foundation.Image
-import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxWidth
@@ -11,10 +10,11 @@ import androidx.compose.foundation.layout.width
 import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.clip
 import androidx.compose.ui.unit.dp
-import com.cairosquad.evolvefit.design_system.text_styles.TextStyle
+import com.cairosquad.evolvefit.design_system.theme.Theme
 import evolvefit.composeapp.generated.resources.Res
 import evolvefit.composeapp.generated.resources.im_default_image
 import org.jetbrains.compose.resources.DrawableResource
@@ -25,28 +25,35 @@ import org.jetbrains.compose.ui.tooling.preview.Preview
 fun ExerciseCard(
     title: String,
     time: String,
-    img: DrawableResource = Res.drawable.im_default_image,
-    modifier: Modifier = Modifier
+    modifier: Modifier = Modifier,
+    img: DrawableResource = Res.drawable.im_default_image
 ) {
     Row(
         modifier = modifier.fillMaxWidth().height(68.dp),
-        horizontalArrangement = Arrangement.Start
+        verticalAlignment = Alignment.CenterVertically
     ) {
         Image(
             painter = painterResource(img),
             contentDescription = "Exercise Image",
             Modifier
                 .width(88.dp)
-                .clip(RoundedCornerShape(16.dp))
+                .clip(RoundedCornerShape(8.dp))
         )
         Column(
             modifier = Modifier.weight(1f)
-
-                .padding(start = 12.dp, top = 12.5.dp, bottom = 12.5.dp),
-            verticalArrangement = Arrangement.SpaceEvenly
+                .padding(start = 12.dp),
         ) {
-            Text(text = title, style = TextStyle().title.mediumMedium14)
-            Text(text = time, style = TextStyle().label.smallRegular12)
+            Text(
+                text = title,
+                style = Theme.textStyle.body.mediumMedium14,
+                color = Theme.color.surfaces.onSurfaceContainer
+            )
+            Text(
+                text = time,
+                style = Theme.textStyle.label.smallRegular12,
+                color = Theme.color.surfaces.onSurfaceVariant,
+                modifier = Modifier.padding(top = 12.dp)
+            )
         }
     }
 }
