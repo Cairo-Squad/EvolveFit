5806f5550e7885e2fef2427dea831285543c07d6
NadeenXo <nadeenserag@gmail.com>
2025-07-31 16:41:36 +0300

    refactor: edit some properties

--- composeApp/src/commonMain/kotlin/com/cairosquad/evolvefit/design_system/composables/MealHistoryCard.kt
+++ composeApp/src/commonMain/kotlin/com/cairosquad/evolvefit/design_system/composables/MealHistoryCard.kt
@@ -1,23 +1,26 @@
 package com.cairosquad.evolvefit.design_system.composables
 
-import androidx.compose.foundation.Image
+import androidx.compose.foundation.background
+import androidx.compose.foundation.border
 import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxWidth
-import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.shape.CircleShape
+import androidx.compose.material3.Icon
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.clip
 import androidx.compose.ui.unit.dp
 import com.cairosquad.evolvefit.design_system.text_styles.TextStyle
 import com.cairosquad.evolvefit.design_system.theme.Theme
 import evolvefit.composeapp.generated.resources.Res
-import evolvefit.composeapp.generated.resources.im_default_image
+import evolvefit.composeapp.generated.resources.ic_coffee
 import org.jetbrains.compose.resources.DrawableResource
 import org.jetbrains.compose.resources.painterResource
 import org.jetbrains.compose.ui.tooling.preview.Preview
@@ -28,25 +31,37 @@ fun MealHistoryCard(
     dateTime: String,
     calories: Int,
     mealType: String,
-    icon: DrawableResource = Res.drawable.im_default_image,
-    modifier: Modifier = Modifier
+    icon: DrawableResource,
+    modifier: Modifier = Modifier,
+    calorieUnit: String = "Kcal"
 ) {
     Row(
-        modifier = modifier.fillMaxWidth().height(68.dp),
-        horizontalArrangement = Arrangement.Start
+        modifier = modifier.fillMaxWidth().padding(16.dp),
+        horizontalArrangement = Arrangement.Center,
+        verticalAlignment = Alignment.CenterVertically
     ) {
-        Image(
-            painter = painterResource(icon),
-            contentDescription = "Image",
+        Box(
             Modifier
                 .padding(vertical = 4.dp)
                 .size(48.dp)
-                .clip(CircleShape)
-        )
+                .border(
+                    width = 1.dp,
+                    color = Theme.color.surfaces.outlineVariant,
+                    shape = CircleShape
+                ).clip(CircleShape)
+                .background(color = Theme.color.surfaces.surfaceContainer),
+            contentAlignment = Alignment.Center
+        ) {
+            Icon(
+                painter = painterResource(icon),
+                contentDescription = "Meal Type",
+                modifier = Modifier.padding(12.dp),
+                tint = Theme.color.brand.primary
+            )
+        }
         Column(
             modifier = Modifier.weight(1f)
-                .padding(start = 12.dp, top = 8.5.dp, bottom = 8.5.dp),
-            verticalArrangement = Arrangement.SpaceEvenly
+                .padding(start = 12.dp, top = 4.5.dp, bottom = 4.5.dp),
         ) {
             Text(
                 text = title,
@@ -64,13 +79,13 @@ fun MealHistoryCard(
             modifier = Modifier.padding(start = 8.dp, top = 11.5.dp, bottom = 11.5.dp)
         ) {
             Text(
-                text = "$calories Kcal",
-                style = TextStyle().title.largeBold14,
+                text = "$calories $calorieUnit",
+                style = Theme.textStyle.title.largeBold14,
                 color = Theme.color.brand.primary
             )
             Text(
                 text = mealType,
-                style = TextStyle().label.smallRegular12,
+                style = Theme.textStyle.body.smallRegular10,
                 color = Theme.color.surfaces.onSurfaceVariant,
                 modifier = Modifier.padding(top = 4.dp)
             )
@@ -85,6 +100,7 @@ private fun MealHistoryCardPreview() {
         title = "Oatmeal with Fruits Squats",
         dateTime = "Today, 7:30 AM",
         calories = 320,
-        mealType = "Breakfast"
+        mealType = "Breakfast",
+        icon = Res.drawable.ic_coffee,
     )
 }
