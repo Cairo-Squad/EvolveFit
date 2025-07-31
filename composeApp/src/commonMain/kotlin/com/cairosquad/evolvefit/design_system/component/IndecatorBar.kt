c6774ad3158e1afb3fde07f4341efc595a1add48
Menna Srour <mennasrour441@gmail.com>
2025-07-31 14:38:13 +0300

    refactor: add padding for IndecatorBar

--- composeApp/src/commonMain/kotlin/com/cairosquad/evolvefit/design_system/component/IndecatorBar.kt
+++ composeApp/src/commonMain/kotlin/com/cairosquad/evolvefit/design_system/component/IndecatorBar.kt
@@ -9,6 +9,7 @@ import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.width
 import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material3.Icon
@@ -34,7 +35,7 @@ fun IndecatorBar(
     contentColor: Color
 ) {
     Row(
-        modifier = Modifier.fillMaxWidth(),
+        modifier = Modifier.fillMaxWidth().padding(horizontal = 16.dp),
         verticalAlignment = Alignment.CenterVertically
     ) {
         IconButton(onClick = onBackClick) {
