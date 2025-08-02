9eefa95c28a8deb30c2893b60355c2f47e82ff62
gehadepada <gehadepada@gmail.com>
2025-08-02 04:28:47 +0300

    feat : add variable needed to RegisterScreenState

--- composeApp/src/commonMain/kotlin/com/cairosquad/evolvefit/viewmodel/register/RegisterScreenState.kt
+++ composeApp/src/commonMain/kotlin/com/cairosquad/evolvefit/viewmodel/register/RegisterScreenState.kt
@@ -3,4 +3,6 @@ package com.cairosquad.evolvefit.viewmodel.register
 data class RegisterScreenState(
     val currentStep: Int = 0,
     val nextButtonEnabled: Boolean = true,
+    val selectedHeight : Float =0F,
+    val selectedWeight :Float =0F,
 )
