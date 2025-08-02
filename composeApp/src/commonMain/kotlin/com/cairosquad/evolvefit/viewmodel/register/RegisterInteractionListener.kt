9f9dfc3061774c804efbfb1f3e578820bbc4b912
gehadepada <gehadepada@gmail.com>
2025-08-02 04:30:56 +0300

    feat : user interactions needed to RegisterInteractionListener

--- composeApp/src/commonMain/kotlin/com/cairosquad/evolvefit/viewmodel/register/RegisterInteractionListener.kt
+++ composeApp/src/commonMain/kotlin/com/cairosquad/evolvefit/viewmodel/register/RegisterInteractionListener.kt
@@ -3,4 +3,6 @@ package com.cairosquad.evolvefit.viewmodel.register
 interface RegisterInteractionListener {
     fun onClickNext()
     fun onClickBack()
+    fun onHeightChanged(height : Float )
+    fun onWeightChanged(weight : Float)
 }
