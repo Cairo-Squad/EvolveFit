38d10643c6140bae187aa9a0da3158e6c7796349
Elsayed Magdy <saikoo1239@gmail.com>
2025-08-02 11:29:10 +0300

    feat : create workout days content screen

---
+++ composeApp/src/commonMain/kotlin/com/cairosquad/evolvefit/ui/screen/onBoarding/WorkoutDaysContent.kt
@@ -0,0 +1,71 @@
+package com.cairosquad.evolvefit.ui.screen.onBoarding
+
+import androidx.compose.foundation.clickable
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.PaddingValues
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.lazy.grid.GridCells
+import androidx.compose.foundation.lazy.grid.LazyVerticalGrid
+import androidx.compose.foundation.lazy.grid.items
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.remember
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.unit.dp
+import com.cairosquad.evolvefit.design_system.component.Chip
+import com.cairosquad.evolvefit.design_system.theme.AppTheme
+import com.cairosquad.evolvefit.design_system.theme.Theme
+import org.jetbrains.compose.ui.tooling.preview.Preview
+import kotlin.collections.get
+
+@Composable
+fun WorkoutDaysContent(modifier: Modifier = Modifier) {
+    val weekdays = remember {
+        listOf(
+            "Saturday",
+            "Sunday",
+            "Monday",
+            "Tuesday",
+            "Wednesday",
+            "Thursday",
+            "Friday"
+        )
+    }
+    Column(
+        modifier = modifier
+            .padding(horizontal = 16.dp)
+            .fillMaxSize()
+    ) {
+        Text(
+            text = "Workout Days",
+            color = Theme.color.surfaces.onSurface,
+            style = Theme.textStyle.headline.mediumMedium18,
+        )
+        Text(
+            modifier = Modifier.padding(top = 8.dp),
+            text = "How often would you like to workout?",
+            color = Theme.color.surfaces.onSurfaceVariant,
+            style = Theme.textStyle.label.smallRegular14,
+        )
+        LazyVerticalGrid(
+            modifier = modifier.padding(top = 24.dp).fillMaxSize(),
+            columns = GridCells.Adaptive(minSize = 101.33.dp),
+            horizontalArrangement = Arrangement.spacedBy(12.dp),
+            verticalArrangement = Arrangement.spacedBy(12.dp)
+        ) {
+            items(weekdays) {
+
+            }
+        }
+    }
+}
+
+@Preview
+@Composable
+fun WorkoutDaysContentPreview() {
+    AppTheme (isDarkTheme = true) {
+        WorkoutDaysContent()
+    }}
