1faa70a2006241afc1603e90c0415c85df1ff9ba
Elsayed Magdy <saikoo1239@gmail.com>
2025-08-02 11:36:33 +0300

    feat : setup notification settings content

---
+++ composeApp/src/commonMain/kotlin/com/cairosquad/evolvefit/ui/screen/onBoarding/NotificationSettingsContent.kt
@@ -0,0 +1,9 @@
+package com.cairosquad.evolvefit.ui.screen.onBoarding
+
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+
+@Composable
+fun NotificationSettingsContent(modifier: Modifier= Modifier){
+
+}
