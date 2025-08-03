cf366d81b5e14643a56b9dad136c950d4c1e4875
Elsayed Magdy <saikoo1239@gmail.com>
2025-08-04 02:31:18 +0300

    refactor : add bottom sheet actions

--- composeApp/src/commonMain/kotlin/com/cairosquad/evolvefit/viewmodel/nutrition/NutritionInteractionListener.kt
+++ composeApp/src/commonMain/kotlin/com/cairosquad/evolvefit/viewmodel/nutrition/NutritionInteractionListener.kt
@@ -2,7 +2,11 @@ package com.cairosquad.evolvefit.viewmodel.nutrition
 
 interface NutritionInteractionListener {
     fun onAddWaterClicked()
-    fun onAddMealClicked()
+    fun onDismissWaterClicked()
+    fun onShowAddMealSheetClicked()
+    fun onConfirmAddMealClicked()
+    fun onDismissMealClicked()
+    fun onSnackBarHided()
     fun onViewAllSuggestedMealsClicked()
     fun onSuggestedMealClicked(mealId: Long)
     fun onViewAllMealHistoryClicked()
