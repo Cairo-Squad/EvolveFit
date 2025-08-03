5d73cafaf1c7734a59c8824a16039cf38260d808
Elsayed Magdy <saikoo1239@gmail.com>
2025-08-04 02:28:20 +0300

    refactor : add nutrition viewmodel di

--- composeApp/src/commonMain/kotlin/com/cairosquad/evolvefit/di/viewModelModule.kt
+++ composeApp/src/commonMain/kotlin/com/cairosquad/evolvefit/di/viewModelModule.kt
@@ -1,9 +1,11 @@
 package com.cairosquad.evolvefit.di
 
+import com.cairosquad.evolvefit.viewmodel.nutrition.NutritionViewModel
 import com.cairosquad.evolvefit.viewmodel.register.RegisterViewModel
 import org.koin.core.module.dsl.viewModelOf
 import org.koin.dsl.module
 
 val viewModelModule = module {
     viewModelOf(::RegisterViewModel)
+    viewModelOf(::NutritionViewModel)
 }
