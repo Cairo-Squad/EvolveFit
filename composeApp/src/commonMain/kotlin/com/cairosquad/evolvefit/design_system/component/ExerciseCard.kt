05f6bb39dfc8879cafd02e23b9becbd5c49042e4
Al-Qassim <71926296+Al-Qassim@users.noreply.github.com>
2025-08-03 23:56:37 +0300

    use network image for the exercise card

--- composeApp/src/commonMain/kotlin/com/cairosquad/evolvefit/design_system/component/ExerciseCard.kt
+++ composeApp/src/commonMain/kotlin/com/cairosquad/evolvefit/design_system/component/ExerciseCard.kt
@@ -1,6 +1,5 @@
 package com.cairosquad.evolvefit.design_system.component
 
-import androidx.compose.foundation.Image
 import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
@@ -14,14 +13,10 @@ import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.clip
-import androidx.compose.ui.layout.ContentScale
 import androidx.compose.ui.unit.dp
 import com.cairosquad.evolvefit.design_system.theme.AppTheme
 import com.cairosquad.evolvefit.design_system.theme.Theme
-import evolvefit.composeapp.generated.resources.Res
-import evolvefit.composeapp.generated.resources.im_default_image
-import org.jetbrains.compose.resources.DrawableResource
-import org.jetbrains.compose.resources.painterResource
+import com.cairosquad.evolvefit.design_system.util.NetworkImage
 import org.jetbrains.compose.ui.tooling.preview.Preview
 
 @Composable
@@ -30,19 +25,18 @@ fun ExerciseCard(
     time: String,
     modifier: Modifier = Modifier,
     unit: String = "Seconds",
-    img: DrawableResource = Res.drawable.im_default_image
+    model: String
 ) {
     Row(
         modifier = modifier.fillMaxWidth(),
         verticalAlignment = Alignment.CenterVertically
     ) {
-        Image(
-            painter = painterResource(img),
-            contentDescription = "Exercise Image",
-            contentScale = ContentScale.Crop,
+        NetworkImage(
             modifier = Modifier
                 .size(width = 88.dp, height = 68.dp)
-                .clip(RoundedCornerShape(8.dp))
+                .clip(RoundedCornerShape(8.dp)),
+            model = model,
+            contentDescription = "Exercise Image",
         )
         Column(
             modifier = Modifier.weight(1f)
@@ -66,13 +60,13 @@ fun ExerciseCard(
 @Preview
 @Composable
 private fun ExerciseCardPreview() {
-    AppTheme(isDarkTheme = false) {
+    AppTheme(isDarkTheme = true) {
         Box(
             Modifier
                 .background(Theme.color.surfaces.surface)
                 .padding(16.dp)
         ) {
-            ExerciseCard(title = "Bodyweight Squats", time = "30")
+            ExerciseCard(title = "Bodyweight Squats", time = "30", model = "")
         }
     }
 }
