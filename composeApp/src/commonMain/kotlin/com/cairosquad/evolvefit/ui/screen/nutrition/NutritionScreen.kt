fa00f281e6e4018a75c01fc42fc681af3f233822
Elsayed Magdy <saikoo1239@gmail.com>
2025-08-04 17:02:17 +0300

    refactor :implement effects

--- composeApp/src/commonMain/kotlin/com/cairosquad/evolvefit/ui/screen/nutrition/NutritionScreen.kt
+++ composeApp/src/commonMain/kotlin/com/cairosquad/evolvefit/ui/screen/nutrition/NutritionScreen.kt
@@ -1,22 +1,6 @@
 package com.cairosquad.evolvefit.ui.screen.nutrition
 
-import androidx.compose.foundation.background
-import androidx.compose.foundation.clickable
-import androidx.compose.foundation.layout.Arrangement
-import androidx.compose.foundation.layout.Column
-import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.layout.padding
-import androidx.compose.foundation.layout.statusBarsPadding
-import androidx.compose.foundation.layout.width
-import androidx.compose.foundation.lazy.LazyRow
-import androidx.compose.material3.Button
-import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
-import androidx.compose.ui.Alignment
-import androidx.compose.ui.Modifier
-import androidx.compose.ui.unit.dp
-import com.cairosquad.evolvefit.design_system.composables.MealCard
-import com.cairosquad.evolvefit.design_system.theme.Theme
 import com.cairosquad.evolvefit.ui.screen.nutrition.content.NutritionScreenContent
 import com.cairosquad.evolvefit.ui.util.ObserveAsEffect
 import com.cairosquad.evolvefit.viewmodel.nutrition.NutritionEffect
@@ -31,53 +15,18 @@ fun NutritionScreen(
     nutritionViewModel: NutritionViewModel = koinViewModel()) {
     ObserveAsEffect(nutritionViewModel.effect) { effect ->
         when (effect) {
-            NutritionEffect.NavigateToMealHistory -> {}
-            is NutritionEffect.NavigateToSuggestedMealDetails -> {}
-            NutritionEffect.NavigateToSuggestedMeals -> {}
+            NutritionEffect.NavigateToMealHistory -> {
+                navigateToMealsHistory()
+            }
+
+            is NutritionEffect.NavigateToSuggestedMealDetails -> {
+                navigateToMealDetails(effect.mealId)
+            }
+
+            NutritionEffect.NavigateToSuggestedMeals -> {
+                navigateToSuggestedMeals()
+            }
         }
     }
     NutritionScreenContent(nutritionViewModel)
-
-    Column(
-        modifier = Modifier
-            .fillMaxSize()
-            .background(color = Theme.color.surfaces.surface)
-            .statusBarsPadding(),
-        horizontalAlignment = Alignment.CenterHorizontally,
-        verticalArrangement = Arrangement.spacedBy(12.dp)
-    ) {
-
-        Text(
-            modifier = Modifier.padding(12.dp),
-            text = "this is the nutrition screen", color = Theme.color.surfaces.onSurface
-        )
-
-        Button(
-            onClick = navigateToSuggestedMeals
-        ) {
-            Text("Suggested Meals")
-        }
-
-        Button(
-            onClick = navigateToMealsHistory
-        ) {
-            Text("Meals History")
-        }
-
-        LazyRow(
-
-            horizontalArrangement = Arrangement.spacedBy(12.dp),
-        ) {
-            items(20) { index ->
-                MealCard(
-                    modifier = Modifier
-                        .width(158.dp)
-                        .clickable(onClick = { navigateToMealDetails((index + 1).toLong()) })
-                    ,
-                    title = "Meal ${index + 1}",
-                    mealType = "Lunch",
-                    calories = 350,
-                )
-            }
-        }
-}}
+}
