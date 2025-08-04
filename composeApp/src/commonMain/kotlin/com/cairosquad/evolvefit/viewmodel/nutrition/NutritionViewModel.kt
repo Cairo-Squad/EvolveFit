5250bbe6d629af209b130a0856799cb45ba7e7a3
Elsayed Magdy <saikoo1239@gmail.com>
2025-08-04 12:05:41 +0300

    refactor :add actions dropped menu

--- composeApp/src/commonMain/kotlin/com/cairosquad/evolvefit/viewmodel/nutrition/NutritionViewModel.kt
+++ composeApp/src/commonMain/kotlin/com/cairosquad/evolvefit/viewmodel/nutrition/NutritionViewModel.kt
@@ -29,6 +29,18 @@ class NutritionViewModel() : BaseViewModel<NutritionScreenState, NutritionEffect
         updateState { it.copy(isAddMealSnackBarVisible = false) }
     }
 
+    override fun onDroppedMenuClick() {
+        updateState { it.copy(isDroppedMenuVisible = true) }
+    }
+
+    override fun onMenuItemClick() {
+        TODO("Not yet implemented")
+    }
+
+    override fun onMealTypeSelected(mealType:  NutritionScreenState.MealType) {
+        updateState { it.copy(mealTypeSelected = mealType) }
+    }
+
     override fun onViewAllSuggestedMealsClicked() {
         sendEffect(NutritionEffect.NavigateToSuggestedMeals)
     }
