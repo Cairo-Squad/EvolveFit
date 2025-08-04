f518c65b4afc34ca98b899dce2861ff7f7a89e0b
Zyad <zyadhammad531@gmail.com>
2025-08-04 19:40:24 +0300

    feature: finish image picker

--- composeApp/src/commonMain/kotlin/com/cairosquad/evolvefit/ui/component/ImagePicker.kt
+++ composeApp/src/commonMain/kotlin/com/cairosquad/evolvefit/ui/component/ImagePicker.kt
@@ -1,13 +1,31 @@
 package com.cairosquad.evolvefit.ui.component
 
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.LaunchedEffect
 import androidx.compose.runtime.rememberCoroutineScope
 import com.cairosquad.evolvefit.viewmodel.onboarding.models.UiImage
+import io.github.vinceglb.filekit.FileKit
+import io.github.vinceglb.filekit.dialogs.FileKitMode
+import io.github.vinceglb.filekit.dialogs.FileKitType
+import io.github.vinceglb.filekit.dialogs.compose.rememberFilePickerLauncher
 
 @Composable
 fun ImagePicker(
     onImageRetrieved: (UiImage) -> Unit,
 ) {
-    val scope = rememberCoroutineScope()
 
+    val launcher = rememberFilePickerLauncher(
+        mode = FileKitMode.Single,
+        type = FileKitType.Image
+    ) { file ->
+        file?.let {
+        onImageRetrieved(
+            UiImage.ImageFile(file)
+        )
+            }
+    }
+
+    LaunchedEffect(true) {
+        launcher.launch()
+    }
 }
