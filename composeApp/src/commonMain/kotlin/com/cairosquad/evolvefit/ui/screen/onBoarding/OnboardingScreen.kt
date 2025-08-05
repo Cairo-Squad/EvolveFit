8a994703a6a3c0ca5329a1bcdfa0a32b9efa1650
Mostafa Mohamed <Mostafa0Mohamed2002@gmail.com>
2025-08-05 22:19:43 +0300

    Refactor: Remove redundant newline in OnboardingScreen

--- composeApp/src/commonMain/kotlin/com/cairosquad/evolvefit/ui/screen/onBoarding/OnboardingScreen.kt
+++ composeApp/src/commonMain/kotlin/com/cairosquad/evolvefit/ui/screen/onBoarding/OnboardingScreen.kt
@@ -55,7 +55,6 @@ fun OnboardingScreen(
             OnboardingScreenEffect.NavigateToLogin -> {
                 navigateToLogin()
             }
-
             OnboardingScreenEffect.NavigateToRegister -> {
                 navigateToRegister()
             }
