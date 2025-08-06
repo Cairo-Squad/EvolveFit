8478135eef4d11de5a91abf0571748306d5d1f96
Elsayed Magdy <saikoo1239@gmail.com>
2025-08-06 12:22:00 +0300

    refactor :add string resources to screen your tools

--- composeApp/src/commonMain/kotlin/com/cairosquad/evolvefit/ui/screen/register/content/RegisterScreenContentSelectYourTools.kt
+++ composeApp/src/commonMain/kotlin/com/cairosquad/evolvefit/ui/screen/register/content/RegisterScreenContentSelectYourTools.kt
@@ -6,21 +6,22 @@ import androidx.compose.foundation.layout.PaddingValues
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.lazy.LazyColumn
-import androidx.compose.foundation.lazy.grid.GridCells
-import androidx.compose.foundation.lazy.grid.LazyVerticalGrid
-import androidx.compose.foundation.lazy.grid.items
 import androidx.compose.foundation.lazy.items
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
-import androidx.compose.runtime.remember
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.unit.dp
 import com.cairosquad.evolvefit.design_system.component.CheckboxItem
 import com.cairosquad.evolvefit.design_system.component.CheckboxStyle
-import com.cairosquad.evolvefit.design_system.component.SelectableDayBox
 import com.cairosquad.evolvefit.design_system.theme.Theme
 import com.cairosquad.evolvefit.viewmodel.register.RegisterInteractionListener
 import com.cairosquad.evolvefit.viewmodel.register.RegisterScreenState
+import evolvefit.composeapp.generated.resources.Res
+import evolvefit.composeapp.generated.resources.no_tools_description
+import evolvefit.composeapp.generated.resources.no_tools_title
+import evolvefit.composeapp.generated.resources.or_select_one_or_more
+import evolvefit.composeapp.generated.resources.your_tools_description
+import evolvefit.composeapp.generated.resources.your_tools_title
 import org.jetbrains.compose.resources.stringResource
 
 @Composable
@@ -29,44 +30,41 @@ fun RegisterScreenContentSelectYourTools(
     listener: RegisterInteractionListener,
     modifier: Modifier = Modifier
 ){
-    //val equipments = remember { RegisterScreenState.Equipment.entries.toList() }
     Column(
         modifier = modifier
             .padding(top = 16.dp)
             .fillMaxSize()
     ) {
-        OnboardingHeader(
-            title = "Your Tools",
-            description = "Select with tools you have, and we’ll tailor your custom plan to reflect this."
+        RegisterHeader(
+            title =stringResource(Res.string.your_tools_title),
+            description = stringResource(Res.string.your_tools_description)
         )
         CheckboxItem(
             modifier = Modifier.padding(horizontal = 16.dp).padding(top = 16.dp),
-            text = "No tools",
-            description = "Show me bodyweight - only workouts.",
+            text =stringResource(Res.string.no_tools_title),
+            description =stringResource(Res.string.no_tools_description),
             isChecked = state.isNoEquipmentSelected,
             onCheckedChange = { listener.onNoEquipmentSelected() },
-            style = CheckboxStyle.Switch
+            style = CheckboxStyle.Tick
         )
 
         Text(
-            modifier = Modifier.padding(horizontal = 16.dp).padding(top = 24.dp),
-            text = "Or select one or more of the  following:",
+            modifier = Modifier.padding(horizontal = 16.dp).padding(top = 24.dp, bottom = 12.dp),
+            text = stringResource(Res.string.or_select_one_or_more),
             color =Theme.color.surfaces.onSurfaceVariant,
             style = Theme.textStyle.label.smallRegular14,
         )
         LazyColumn(
             modifier = modifier
                 .padding(horizontal = 16.dp)
-                .padding(top = 24.dp)
                 .fillMaxSize(),
             verticalArrangement = Arrangement.spacedBy(12.dp),
-            contentPadding = PaddingValues(vertical = 12.dp)
+            contentPadding = PaddingValues(bottom = 12.dp)
         ) {
             items(state.availableEquipments) { equipment ->
                 val isSelected = state.selectedEquipments.contains(equipment.name)
                 val isEnabled = !state.isNoEquipmentSelected
                 CheckboxItem(
-                    modifier = Modifier.padding(top = 8.dp),
                     text =  equipment.name,
                     isChecked = isSelected,
                     onCheckedChange = {
@@ -74,7 +72,7 @@ fun RegisterScreenContentSelectYourTools(
                             listener.onEquipmentToggled(equipment.name)
                         }
                     },
-                    style = CheckboxStyle.Switch
+                    style = CheckboxStyle.Tick
                 )
             }
         }
