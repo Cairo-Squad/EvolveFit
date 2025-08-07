216bfaa9ca92f6c019fa63eae18e1d70bf21bc69
Zyad Hammad <zyad.nasser@systemira.net>
2025-08-07 16:23:00 +0300

    feature: vertical pager for date picker

--- composeApp/src/commonMain/kotlin/com/cairosquad/evolvefit/ui/component/DatePicker.kt
+++ composeApp/src/commonMain/kotlin/com/cairosquad/evolvefit/ui/component/DatePicker.kt
@@ -3,8 +3,10 @@ package com.cairosquad.evolvefit.ui.component
 import androidx.compose.animation.animateColorAsState
 import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.PaddingValues
 import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.fillMaxHeight
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
@@ -17,7 +19,9 @@ import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
 import androidx.compose.runtime.setValue
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.unit.dp
 import com.cairosquad.evolvefit.design_system.theme.AppTheme
 import com.cairosquad.evolvefit.design_system.theme.Theme
@@ -51,20 +55,29 @@ fun DatePicker(
         VerticalPager(
             state = dayPagerState,
             modifier = Modifier.weight(1f),
-            pageSpacing = 0.dp,
-            contentPadding = PaddingValues(0.dp),
+            pageSpacing = 4.dp,
+            contentPadding = PaddingValues(106.dp),
             pageSize = PageSize.Fixed(40.dp),
-
-            ) { page ->
+        ) { page ->
             val animatedContentColor by animateColorAsState(
                 targetValue = if (page == dayPagerState.currentPage) Theme.color.surfaces.onSurfaceContainer else Theme.color.surfaces.outlineVariant
             )
 
-            Text(
-                text = "${page + 1}",
-                style = Theme.textStyle.body.mediumMedium14, // TODO: There is a missing text style when not selected (Poppins/Title 14 Regular)
-                color = animatedContentColor
-            )
+            Box(
+                modifier = Modifier
+                    .fillMaxHeight()
+                    .background(
+                        color = if (page == dayPagerState.currentPage) Theme.color.surfaces.surfaceContainer else Color.Transparent
+                    ),
+                contentAlignment = Alignment.Center
+            ) {
+                Text(
+                    text = "${page + 1}",
+                    style = Theme.textStyle.body.mediumMedium14, // TODO: There is a missing text style when not selected (Poppins/Title 14 Regular)
+                    color = animatedContentColor,
+                    modifier = Modifier
+                )
+            }
         }
     }
 }
