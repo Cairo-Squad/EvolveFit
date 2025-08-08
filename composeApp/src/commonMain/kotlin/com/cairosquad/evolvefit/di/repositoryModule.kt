d5fa7c5d41c7139c5230f399a5d5158508299d6b
Shahd-Hatem <Shahdhatim863@gmail.com>
2025-08-08 21:54:09 +0300

    update repositoryModule and useCasaModule

--- composeApp/src/commonMain/kotlin/com/cairosquad/evolvefit/di/repositoryModule.kt
+++ composeApp/src/commonMain/kotlin/com/cairosquad/evolvefit/di/repositoryModule.kt
@@ -1,7 +1,20 @@
 package com.cairosquad.evolvefit.di
 
+import com.cairosquad.evolvefit.Repository.AuthRepositoryImpl
+import com.cairosquad.evolvefit.Repository.WorkoutRepositoryImpl
+import com.cairosquad.evolvefit.domain.AuthRemoteDataSource
+import com.cairosquad.evolvefit.domain.AuthRepository
+import com.cairosquad.evolvefit.domain.WorkoutRemoteDataSource
+import com.cairosquad.evolvefit.domain.WorkoutRepository
+import com.cairosquad.evolvefit.remote.Auth.AuthRemoteDataSourceImp
+import com.cairosquad.evolvefit.remote.provideHttpClient
+import com.cairosquad.evolvefit.remote.workOut.WorkoutRemoteDataSourceImpl
 import org.koin.dsl.module
 
 val repositoryModule = module {
-    // TODO: instantiate repositories
+    single<WorkoutRepository> { WorkoutRepositoryImpl(get()) }
+    single<WorkoutRemoteDataSource>{ WorkoutRemoteDataSourceImpl(get()) }
+    single<AuthRemoteDataSource> { AuthRemoteDataSourceImp(get()) }
+    single<AuthRepository> { AuthRepositoryImpl(get(),get()) }
+    single { provideHttpClient(get(),get()) }
 }
