c9995beeab0202c28c9d2d61aa6a709c5a44ca13
Hager Abobakr <hagarabobakr1@gmail.com>
2025-08-08 23:04:00 +0300

    implement SignUpPromptRow

---
+++ composeApp/src/commonMain/kotlin/com/cairosquad/evolvefit/ui/screen/login/content/SignUpPromptRow.kt
@@ -0,0 +1,37 @@
+package com.cairosquad.evolvefit.ui.screen.login.content
+
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.unit.dp
+import com.cairosquad.evolvefit.design_system.theme.Theme
+import evolvefit.composeapp.generated.resources.Res
+import evolvefit.composeapp.generated.resources.do_not_have_an_account
+import evolvefit.composeapp.generated.resources.join_now
+import org.jetbrains.compose.resources.stringResource
+
+@Composable
+ fun SignUpPromptRow() {
+    Row(
+        modifier = Modifier
+            .padding(horizontal = 74.dp)
+            .padding(bottom = 24.dp),
+        horizontalArrangement = Arrangement.spacedBy(4.dp),
+        verticalAlignment = Alignment.CenterVertically
+    ) {
+        Text(
+            text = stringResource(Res.string.do_not_have_an_account),
+            style = Theme.textStyle.label.mediumMedium14,
+            color = Theme.color.surfaces.onSurfaceVariant,
+        )
+        Text(
+            text = stringResource(Res.string.join_now),
+            style = Theme.textStyle.label.mediumMedium16,
+            color = Theme.color.brand.primary,
+        )
+    }
+}
