ae14ec416a9be51f561c31e7f4e1915b5afbfd0e
Shahd-Hatem <Shahdhatim863@gmail.com>
2025-08-08 21:47:21 +0300

    update Gradle

--- composeApp/build.gradle.kts
+++ composeApp/build.gradle.kts
@@ -1,3 +1,4 @@
+import org.gradle.kotlin.dsl.implementation
 import org.jetbrains.kotlin.gradle.ExperimentalKotlinGradlePluginApi
 import org.jetbrains.kotlin.gradle.dsl.JvmTarget
 
@@ -70,7 +71,12 @@ kotlin {
             implementation(libs.filekit.dialogs.compose)
             implementation(libs.filekit.coil)
 
-
+            implementation(libs.ktor.client.core)
+            implementation(libs.ktor.client.content.negotiation)
+            implementation(libs.ktor.serialization.kotlinx.json)
+            implementation(libs.ktor.client.logging)
+            implementation("com.russhwolf:multiplatform-settings-no-arg:1.3.0")
+            implementation("io.ktor:ktor-client-auth:2.3.2")
         }
         commonTest.dependencies {
             implementation(libs.kotlin.test)
