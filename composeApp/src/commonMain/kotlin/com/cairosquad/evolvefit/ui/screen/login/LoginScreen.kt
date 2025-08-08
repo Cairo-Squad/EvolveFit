c31c62833fea89fcba56b1860ae6e15458addc48
Elsayed Magdy <saikoo1239@gmail.com>
2025-08-08 19:36:11 +0300

    refactor : add string resources and format lines

--- composeApp/src/commonMain/kotlin/com/cairosquad/evolvefit/ui/screen/login/LoginScreen.kt
+++ composeApp/src/commonMain/kotlin/com/cairosquad/evolvefit/ui/screen/login/LoginScreen.kt
@@ -6,14 +6,17 @@ import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.layout.statusBarsPadding
+import androidx.compose.material3.Icon
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.collectAsState
 import androidx.compose.runtime.getValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.unit.dp
 import com.cairosquad.evolvefit.design_system.component.PrimaryButton
 import com.cairosquad.evolvefit.design_system.composables.InputField
+import com.cairosquad.evolvefit.design_system.theme.Theme
 import com.cairosquad.evolvefit.ui.screen.register.content.RegisterHeader
 import com.cairosquad.evolvefit.ui.util.ObserveAsEffect
 import com.cairosquad.evolvefit.viewmodel.login.LoginEffect
@@ -21,6 +24,8 @@ import com.cairosquad.evolvefit.viewmodel.login.LoginInteractionListener
 import com.cairosquad.evolvefit.viewmodel.login.LoginScreenUiState
 import com.cairosquad.evolvefit.viewmodel.login.LoginViewModel
 import evolvefit.composeapp.generated.resources.Res
+import evolvefit.composeapp.generated.resources.arrow_back_description
+import evolvefit.composeapp.generated.resources.email_placeholder
 import evolvefit.composeapp.generated.resources.ic_app_logo
 import evolvefit.composeapp.generated.resources.ic_back
 import evolvefit.composeapp.generated.resources.ic_lock
@@ -28,6 +33,10 @@ import evolvefit.composeapp.generated.resources.ic_profile
 import evolvefit.composeapp.generated.resources.ic_visibility_off
 import evolvefit.composeapp.generated.resources.ic_visibility_on
 import evolvefit.composeapp.generated.resources.login
+import evolvefit.composeapp.generated.resources.logo_description
+import evolvefit.composeapp.generated.resources.password_placeholder
+import evolvefit.composeapp.generated.resources.welcome_back_description
+import evolvefit.composeapp.generated.resources.welcome_back_title
 import org.jetbrains.compose.resources.painterResource
 import org.jetbrains.compose.resources.stringResource
 import org.koin.compose.viewmodel.koinViewModel
@@ -45,7 +54,6 @@ fun LoginScreen(
             }
         }
     }
-
     LoginScreenContent(state = state, listener = loginViewModel)
 }
 
@@ -54,6 +62,7 @@ private fun LoginScreenContent(
     state: LoginScreenUiState,
     listener: LoginInteractionListener
 ) {
+    // handle it as interaction listener in viewmodel
     val visibilityIcon = if (state.isPasswordVisible) {
         Res.drawable.ic_visibility_on
     } else {
@@ -66,39 +75,41 @@ private fun LoginScreenContent(
         horizontalAlignment = Alignment.CenterHorizontally
     ) {
         Image(
-            painter = painterResource(Res.drawable.ic_back),
-            contentDescription = stringResource(Res.string.arrow_back),
             modifier = Modifier
                 .padding(8.dp)
                 .size(24.dp)
-                .align(Alignment.Start)
+                .align(Alignment.Start),
+            painter = painterResource(Res.drawable.ic_back),
+            contentDescription = stringResource(Res.string.arrow_back_description),
         )
 
-        Image(
-            painter = painterResource(Res.drawable.ic_app_logo),
-            contentDescription = stringResource(Res.string.logo),
+        Icon(
             modifier = Modifier
-                .padding(bottom = 24.dp, top = 20.dp)
-                .size(80.dp)
+                .padding(bottom = 24.dp, top = 20.dp),
+            painter = painterResource(Res.drawable.ic_app_logo),
+            contentDescription = stringResource(Res.string.logo_description),
+            tint = Theme.color.brand.primary,
         )
 
         RegisterHeader(
-            title = stringResource(Res.string.welcome_back),
-            description = stringResource(Res.string.welcome_description),
             modifier = Modifier
-                .padding(bottom = 24.dp)
+                .padding(bottom = 24.dp),
+            title = stringResource(Res.string.welcome_back_title),
+            description = stringResource(Res.string.welcome_back_description),
         )
 
         InputField(
+            modifier = Modifier
+                .padding(bottom = 12.dp),
             value = state.email,
             onValueChange = listener::onEmailChanged,
             placeholder = stringResource(Res.string.email_placeholder),
             leadingIcon = Res.drawable.ic_profile,
-            modifier = Modifier
-                .padding(bottom = 12.dp)
         )
 
         InputField(
+            modifier = Modifier
+                .padding(bottom = 68.dp),
             value = state.password,
             onValueChange = listener::onPasswordChanged,
             placeholder = stringResource(Res.string.password_placeholder),
@@ -106,9 +117,6 @@ private fun LoginScreenContent(
             leadingIcon = Res.drawable.ic_lock,
             trailingIcon = visibilityIcon,
             onTrailingIconClick = listener::onTogglePasswordVisibility,
-            modifier = Modifier
-                .padding(bottom = 68.dp)
-
         )
         PrimaryButton(
             text = stringResource(Res.string.login),
