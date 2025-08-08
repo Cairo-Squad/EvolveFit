7664b0cde493333047531c1293711f8c4dcb4666
Shahd-Hatem <Shahdhatim863@gmail.com>
2025-08-08 21:46:03 +0300

    add AuthRemoteDataSource

---
+++ composeApp/src/commonMain/kotlin/com/cairosquad/evolvefit/remote/Auth/AuthRemoteDataSourceImp.kt
@@ -0,0 +1,37 @@
+package com.cairosquad.evolvefit.remote.Auth
+
+import com.cairosquad.evolvefit.domain.AuthRemoteDataSource
+import com.cairosquad.evolvefit.remote.model.LoginRequest
+import com.cairosquad.evolvefit.remote.model.LoginResponse
+import com.cairosquad.evolvefit.remote.model.RefreshRequest
+import com.cairosquad.evolvefit.remote.model.RegisterRequest
+import io.ktor.client.HttpClient
+import io.ktor.client.call.body
+import io.ktor.client.request.post
+import io.ktor.client.request.setBody
+import io.ktor.http.ContentType
+import io.ktor.http.contentType
+
+class AuthRemoteDataSourceImp(private val client: HttpClient) : AuthRemoteDataSource {
+    override suspend fun login(email: String, password: String): LoginResponse {
+        return client.post("auth/login") {
+            contentType(ContentType.Application.Json)
+            setBody(LoginRequest(email, password))
+        }.body()
+    }
+
+    override suspend fun register(request: RegisterRequest) {
+        client.post("auth/signup") {
+            contentType(ContentType.Application.Json)
+            setBody(request)
+        }
+    }
+
+    override suspend fun refreshToken(refreshToken: String): LoginResponse {
+        return client.post("auth/refresh") {
+            contentType(ContentType.Application.Json)
+            setBody(RefreshRequest(refreshToken))
+        }.body()
+    }
+}
+
