b5e7de556076e60c69e38e4434c0ea8760c44920
Shahd-Hatem <Shahdhatim863@gmail.com>
2025-08-08 21:45:22 +0300

    add auth model

---
+++ composeApp/src/commonMain/kotlin/com/cairosquad/evolvefit/remote/model/AuthModel.kt
@@ -0,0 +1,33 @@
+package com.cairosquad.evolvefit.remote.model
+
+import kotlinx.serialization.Serializable
+
+@Serializable
+data class LoginRequest(
+    val email: String,
+    val password: String
+)
+
+@Serializable
+data class LoginResponse(
+    val accessToken: String,
+    val refreshToken: String
+)
+
+@Serializable
+data class RegisterRequest(
+    val name: String,
+    val email: String,
+    val dateOfBirth: String,
+    val password: String,
+    val gender: String,
+    val unit: String,
+    val height: Float,
+    val weight: Float,
+    val goal: String,
+    val tools: List<String>,
+    val workoutDays: List<String>
+)
+
+@Serializable
+data class RefreshRequest(val refreshToken: String)
