e2edeb4741b8ee4d4f2d4663c5e4e6f542fabf21
Shahd-Hatem <Shahdhatim863@gmail.com>
2025-08-08 22:34:43 +0300

    add mapper to domain

---
+++ composeApp/src/commonMain/kotlin/com/cairosquad/evolvefit/viewmodel/register/mapper.kt
@@ -0,0 +1,49 @@
+package com.cairosquad.evolvefit.viewmodel.register
+
+import com.cairosquad.evolvefit.entity.FitnessGoal
+import com.cairosquad.evolvefit.entity.Gender
+import com.cairosquad.evolvefit.entity.MeasurementUnit
+import com.cairosquad.evolvefit.entity.Tool
+import com.cairosquad.evolvefit.entity.WorkoutDay
+
+fun List<String>.toToolsDomain(state: RegisterScreenState): List<Tool> {
+    return this.mapNotNull { selectedId ->
+        state.availableEquipments.find { it.name == selectedId }?.let {
+            Tool(id = selectedId, name = it.name)
+        }
+    }
+}
+
+fun RegisterScreenState.WorkoutDay.toDomain(): WorkoutDay {
+    return when (this) {
+        RegisterScreenState.WorkoutDay.MONDAY -> WorkoutDay.MONDAY
+        RegisterScreenState.WorkoutDay.TUESDAY -> WorkoutDay.TUESDAY
+        RegisterScreenState.WorkoutDay.WEDNESDAY -> WorkoutDay.WEDNESDAY
+        RegisterScreenState.WorkoutDay.THURSDAY -> WorkoutDay.THURSDAY
+        RegisterScreenState.WorkoutDay.FRIDAY -> WorkoutDay.FRIDAY
+        RegisterScreenState.WorkoutDay.SATURDAY -> WorkoutDay.SATURDAY
+        RegisterScreenState.WorkoutDay.SUNDAY -> WorkoutDay.SUNDAY
+    }
+}
+
+fun RegisterScreenState.Goal.toDomain(): FitnessGoal {
+    return when (this) {
+        RegisterScreenState.Goal.LoseWeight -> FitnessGoal.LOSE_WEIGHT
+        RegisterScreenState.Goal.GainWeight -> FitnessGoal.GAIN_WEIGHT
+        RegisterScreenState.Goal.StayInShape -> FitnessGoal.STAY_IN_SHAPE
+    }
+}
+
+fun RegisterScreenState.MeasurementUnit.toDomain(): MeasurementUnit {
+    return when (this) {
+        RegisterScreenState.MeasurementUnit.Metric -> MeasurementUnit.METRIC
+        RegisterScreenState.MeasurementUnit.Imperial -> MeasurementUnit.IMPERIAL
+    }
+}
+
+fun RegisterScreenState.Gender.toDomain(): Gender {
+    return when (this) {
+        RegisterScreenState.Gender.Male -> Gender.MALE
+        RegisterScreenState.Gender.Female -> Gender.FEMALE
+    }
+}
