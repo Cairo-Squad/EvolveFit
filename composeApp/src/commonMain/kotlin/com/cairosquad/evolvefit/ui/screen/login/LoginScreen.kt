6caa47ef0e50bebe460036bf85210a4a66f8d19a
Hager Abobakr <hagarabobakr1@gmail.com>
2025-08-08 18:59:15 +0300

    implement LoginScreen UI
    This commit introduces the UI for the LoginScreen.
    
    - Includes input fields for email and password.
    - Implements password visibility toggle.
    - Adds a login button that is enabled when input is valid.
    - Integrates with `LoginViewModel` for state management and interaction handling.
    - Displays the app logo and a welcome message.
    - Provides navigation to the home screen upon successful login.

--- composeApp/src/commonMain/kotlin/com/cairosquad/evolvefit/ui/screen/login/LoginScreen.kt
+++ composeApp/src/commonMain/kotlin/com/cairosquad/evolvefit/ui/screen/login/LoginScreen.kt
@@ -1,29 +1,119 @@
 package com.cairosquad.evolvefit.ui.screen.login
 
-import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.Image
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.material3.Button
-import androidx.compose.material3.Text
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.layout.statusBarsPadding
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.collectAsState
+import androidx.compose.runtime.getValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.unit.dp
-import com.cairosquad.evolvefit.design_system.theme.Theme
+import com.cairosquad.evolvefit.design_system.component.PrimaryButton
+import com.cairosquad.evolvefit.design_system.composables.InputField
+import com.cairosquad.evolvefit.ui.screen.register.content.RegisterHeader
+import com.cairosquad.evolvefit.ui.util.ObserveAsEffect
+import com.cairosquad.evolvefit.viewmodel.login.LoginEffect
+import com.cairosquad.evolvefit.viewmodel.login.LoginInteractionListener
+import com.cairosquad.evolvefit.viewmodel.login.LoginScreenUiState
+import com.cairosquad.evolvefit.viewmodel.login.LoginViewModel
+import evolvefit.composeapp.generated.resources.Res
+import evolvefit.composeapp.generated.resources.ic_app_logo
+import evolvefit.composeapp.generated.resources.ic_back
+import evolvefit.composeapp.generated.resources.ic_lock
+import evolvefit.composeapp.generated.resources.ic_profile
+import evolvefit.composeapp.generated.resources.ic_visibility_off
+import evolvefit.composeapp.generated.resources.ic_visibility_on
+import evolvefit.composeapp.generated.resources.login
+import org.jetbrains.compose.resources.painterResource
+import org.jetbrains.compose.resources.stringResource
+import org.koin.compose.viewmodel.koinViewModel
 
 @Composable
 fun LoginScreen(
     navigateToApp: () -> Unit,
-){
+    loginViewModel: LoginViewModel = koinViewModel()
+) {
+    val state by loginViewModel.screenState.collectAsState()
+    ObserveAsEffect(loginViewModel.effect) { effect ->
+        when (effect) {
+            LoginEffect.NavigateToHome -> {
+                navigateToApp()
+            }
+        }
+    }
+
+    LoginScreenContent(state = state, listener = loginViewModel)
+}
+
+@Composable
+private fun LoginScreenContent(
+    state: LoginScreenUiState,
+    listener: LoginInteractionListener
+) {
+    val visibilityIcon = if (state.isPasswordVisible) {
+        Res.drawable.ic_visibility_on
+    } else {
+        Res.drawable.ic_visibility_off
+    }
     Column(
-        modifier = Modifier.fillMaxSize(),
-        verticalArrangement = Arrangement.spacedBy(12.dp, Alignment.CenterVertically),
+        modifier = Modifier.fillMaxSize()
+            .statusBarsPadding()
+            .padding(horizontal = 16.dp),
         horizontalAlignment = Alignment.CenterHorizontally
     ) {
-        Button(
-            onClick = navigateToApp
-        ){
-            Text("go to home screen", color = Theme.color.surfaces.onSurface)
-        }
+        Image(
+            painter = painterResource(Res.drawable.ic_back),
+            contentDescription = stringResource(Res.string.arrow_back),
+            modifier = Modifier
+                .padding(8.dp)
+                .size(24.dp)
+                .align(Alignment.Start)
+        )
+
+        Image(
+            painter = painterResource(Res.drawable.ic_app_logo),
+            contentDescription = stringResource(Res.string.logo),
+            modifier = Modifier
+                .padding(bottom = 24.dp, top = 20.dp)
+                .size(80.dp)
+        )
+
+        RegisterHeader(
+            title = stringResource(Res.string.welcome_back),
+            description = stringResource(Res.string.welcome_description),
+            modifier = Modifier
+                .padding(bottom = 24.dp)
+        )
+
+        InputField(
+            value = state.email,
+            onValueChange = listener::onEmailChanged,
+            placeholder = stringResource(Res.string.email_placeholder),
+            leadingIcon = Res.drawable.ic_profile,
+            modifier = Modifier
+                .padding(bottom = 12.dp)
+        )
+
+        InputField(
+            value = state.password,
+            onValueChange = listener::onPasswordChanged,
+            placeholder = stringResource(Res.string.password_placeholder),
+            isPasswordField = !state.isPasswordVisible,
+            leadingIcon = Res.drawable.ic_lock,
+            trailingIcon = visibilityIcon,
+            onTrailingIconClick = listener::onTogglePasswordVisibility,
+            modifier = Modifier
+                .padding(bottom = 68.dp)
+
+        )
+        PrimaryButton(
+            text = stringResource(Res.string.login),
+            isEnabled = state.canSubmit,
+            onClick = listener::onLoginClicked
+        )
     }
 }
