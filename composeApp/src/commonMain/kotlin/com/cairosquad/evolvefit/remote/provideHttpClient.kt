048dc2dd5f7a56daa268cd66cc2a9988c7c63ae0
Shahd-Hatem <Shahdhatim863@gmail.com>
2025-08-08 21:52:07 +0300

    add provideHttpClient to commonMain

---
+++ composeApp/src/commonMain/kotlin/com/cairosquad/evolvefit/remote/provideHttpClient.kt
@@ -0,0 +1,52 @@
+package com.cairosquad.evolvefit.remote
+
+import com.cairosquad.evolvefit.domain.AuthRemoteDataSource
+import com.cairosquad.evolvefit.local.AuthPreferences
+import io.ktor.client.HttpClient
+
+//fun provideHttpClient(
+//    authPreferences: AuthPreferences,
+//    authRemoteDataSource: AuthRemoteDataSource
+//): HttpClient {
+//    return HttpClient {
+//        install(ContentNegotiation) {
+//            json(Json { ignoreUnknownKeys = true })
+//        }
+//        install(Logging) {
+//            level = LogLevel.ALL
+//        }
+//        defaultRequest {
+//            url(
+//                "https://evolve-fit-dev.the-chance.net/"
+//            )
+////                protocol = URLProtocol.HTTPS
+////                host = "evolve-fit-dev.the-chance.net"
+//        }
+//
+//        install(Auth) {
+//            bearer {
+//                loadTokens {
+//                    authPreferences.getAccessToken()?.let { accessToken ->
+//                        authPreferences.getRefreshToken()?.let { refreshToken ->
+//                            BearerTokens(accessToken, refreshToken)
+//                        }
+//                    }
+//                }
+//                refreshTokens {
+//                    val refreshToken = authPreferences.getRefreshToken()
+//                    if (refreshToken != null) {
+//                        val newTokens = authRemoteDataSource.refreshToken(refreshToken)
+//                        authPreferences.saveTokens(newTokens.accessToken, newTokens.refreshToken)
+//                        BearerTokens(newTokens.accessToken, newTokens.refreshToken)
+//                    } else {
+//                        null
+//                    }
+//                }
+//            }
+//        }
+//    }
+//}
+expect fun provideHttpClient(
+    authPreferences: AuthPreferences,
+    authRemoteDataSource: AuthRemoteDataSource
+): HttpClient
