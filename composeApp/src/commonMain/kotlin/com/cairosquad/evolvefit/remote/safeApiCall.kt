32776db780ddc3852846dedfbb39ef07e603d199
Shahd-Hatem <Shahdhatim863@gmail.com>
2025-08-08 21:55:07 +0300

    make safeApiCall to handle https exeptions

---
+++ composeApp/src/commonMain/kotlin/com/cairosquad/evolvefit/remote/safeApiCall.kt
@@ -0,0 +1,9 @@
+package com.cairosquad.evolvefit.remote
+
+suspend inline fun <T> safeApiCall(apiCall: () -> T): T {
+    try {
+        return apiCall()
+    } catch (e: Exception) {
+        throw e
+    }
+}
