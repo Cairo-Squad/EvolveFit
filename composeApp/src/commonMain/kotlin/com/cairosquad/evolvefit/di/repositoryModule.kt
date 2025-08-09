9e7a90d3bbd0646c8c032e5de920b8cb623dfc3f
Shahd-Hatem <Shahdhatim863@gmail.com>
2025-08-09 18:09:05 +0300

    update repositoryModule to inject right provideHttpClient

--- composeApp/src/commonMain/kotlin/com/cairosquad/evolvefit/di/repositoryModule.kt
+++ composeApp/src/commonMain/kotlin/com/cairosquad/evolvefit/di/repositoryModule.kt
@@ -3,14 +3,23 @@ package com.cairosquad.evolvefit.di
 import com.cairosquad.evolvefit.Repository.AuthRepositoryImpl
 import com.cairosquad.evolvefit.domain.AuthRemoteDataSource
 import com.cairosquad.evolvefit.domain.AuthRepository
+import com.cairosquad.evolvefit.local.AuthPreferences
 import com.cairosquad.evolvefit.remote.Auth.AuthRemoteDataSourceImp
 import com.cairosquad.evolvefit.remote.provideHttpClient
+import io.ktor.client.plugins.auth.providers.BearerTokens
 import org.koin.dsl.module
 
 val repositoryModule = module {
-    single { provideHttpClient(get()) }
-//    single<WorkoutRepository> { WorkoutRepositoryImpl(get()) }
-//    single<WorkoutRemoteDataSource>{ WorkoutRemoteDataSourceImpl(get()) }
+    single {
+        provideHttpClient(
+            authPreferences = get(),
+            refreshTokenProvider = { token ->
+                val newTokens = get<AuthRemoteDataSource>().refreshToken(token)
+                get<AuthPreferences>().saveTokens(newTokens.accessToken, newTokens.refreshToken)
+                BearerTokens(newTokens.accessToken, newTokens.refreshToken)
+            }
+        )
+    }
     single<AuthRemoteDataSource> { AuthRemoteDataSourceImp(get()) }
     single<AuthRepository> { AuthRepositoryImpl(get(),get()) }
 }
