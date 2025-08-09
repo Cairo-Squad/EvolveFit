fe37c01ad899edc53929baabcba26b8e99f1086e
Shahd-Hatem <Shahdhatim863@gmail.com>
2025-08-09 18:01:17 +0300

    remove unused files

--- composeApp/src/commonMain/kotlin/com/cairosquad/evolvefit/viewmodel/login/LoginScreenState.kt
+++
@@ -1,9 +0,0 @@
-package com.cairosquad.evolvefit.viewmodel.login
-
-data class LoginScreenState(
-    val isLoading: Boolean = false,
-    val email: String = "",
-    val password: String = "",
-    val emailError: String? = null,
-    val passwordError: String? = null
-)
