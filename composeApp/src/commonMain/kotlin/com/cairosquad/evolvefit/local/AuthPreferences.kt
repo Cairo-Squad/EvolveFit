13a0c3e7e090c958dfe80a108d200ef25712e5c3
Shahd-Hatem <Shahdhatim863@gmail.com>
2025-08-09 17:59:14 +0300

    make tokens as state flow

--- composeApp/src/commonMain/kotlin/com/cairosquad/evolvefit/local/AuthPreferences.kt
+++ composeApp/src/commonMain/kotlin/com/cairosquad/evolvefit/local/AuthPreferences.kt
@@ -1,17 +1,20 @@
 package com.cairosquad.evolvefit.local
 
 import com.russhwolf.settings.Settings
+import kotlinx.coroutines.flow.MutableStateFlow
+import kotlinx.coroutines.flow.StateFlow
 
 class AuthPreferences(private val settings: Settings) {
+
+    private val _accessTokenFlow = MutableStateFlow(getAccessToken())
+    val accessTokenFlow: StateFlow<String?> = _accessTokenFlow
+
     fun saveTokens(access: String, refresh: String) {
         settings.putString("access_token", access)
         settings.putString("refresh_token", refresh)
+        _accessTokenFlow.value = access
     }
 
-//    fun saveAccessToken(token: String) {
-//        settings.putString("access_token", token)
-//    }
-//make save individually
     fun getAccessToken(): String? = settings.getStringOrNull("access_token")
 
     fun getRefreshToken(): String? = settings.getStringOrNull("refresh_token")
@@ -19,5 +22,6 @@ class AuthPreferences(private val settings: Settings) {
     fun clear() {
         settings.remove("access_token")
         settings.remove("refresh_token")
+        _accessTokenFlow.value = null
     }
 }
