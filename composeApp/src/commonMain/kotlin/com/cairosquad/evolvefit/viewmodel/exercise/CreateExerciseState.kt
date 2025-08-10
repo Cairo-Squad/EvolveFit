5796f9a65b20e8b51efdc93bd99902884e6d82a7
Shahd-Hatem <Shahdhatim863@gmail.com>
2025-08-10 23:00:01 +0300

    add CreateExerciseState

---
+++ composeApp/src/commonMain/kotlin/com/cairosquad/evolvefit/viewmodel/exercise/CreateExerciseState.kt
@@ -0,0 +1,37 @@
+package com.cairosquad.evolvefit.viewmodel.exercise
+
+import com.cairosquad.evolvefit.viewmodel.onboarding.models.UiImage
+
+data class CreateExerciseState(
+    val name: String = "",
+    val image: UiImage? = null,
+    val availableEquipments: List<Equipment> = emptyList(),
+    val selectedEquipments: List<Long> = emptyList(),
+    val measurementType: MeasurementType = MeasurementType.DURATION,
+    val measurementValue: Int? = null,
+    val selectedFocusAreas: Set<FocusArea> = emptySet(),
+    val description: String = "",
+    val isSaving: Boolean = false,
+    val showExitBottomSheet: Boolean = false,
+    val errorMessage: String? = null,
+    val isImagePickerOpen: Boolean = false,
+) {
+    data class Equipment(
+        val toolName: String = "",
+        val toolId: Long,
+        val isSelected: Boolean = false
+    )
+
+    enum class FocusArea {
+        Quadriceps,
+        Abs,
+        Calves,
+        LowerBack,
+        Core,
+        Shoulders
+    }
+
+    enum class MeasurementType {
+        DURATION, REPS
+    }
+}
