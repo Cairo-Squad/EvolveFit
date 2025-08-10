74da926991b75327b31671d7a83f5f8183732a43
NadeenXo <nadeenserag@gmail.com>
2025-08-10 21:42:53 +0300

    merged with develop

--- composeApp/src/commonMain/kotlin/com/cairosquad/evolvefit/di/useCaseModule.kt
+++ composeApp/src/commonMain/kotlin/com/cairosquad/evolvefit/di/useCaseModule.kt
@@ -1,14 +1,11 @@
 package com.cairosquad.evolvefit.di
 
-import com.cairosquad.evolvefit.domain.AuthRepository
 import com.cairosquad.evolvefit.domain.usecase.authentication.AuthUseCase
 import com.cairosquad.evolvefit.domain.usecase.workout.ManageWorkoutsUseCase
-import com.cairosquad.evolvefit.repository.FakeAuthRepository
 import org.koin.dsl.module
 
 val useCaseModule = module {
     factory { AuthUseCase(get()) }
-    single<AuthRepository> { FakeAuthRepository() }
     single { AuthUseCase(get()) }
     single { ManageWorkoutsUseCase(get()) }
 }
