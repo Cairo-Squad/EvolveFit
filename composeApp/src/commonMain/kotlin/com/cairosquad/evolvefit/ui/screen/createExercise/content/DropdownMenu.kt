63075f968d1f6ae6e80ca11cd3c41416e258c862
Hager Abobakr <hagarabobakr1@gmail.com>
2025-08-11 04:12:33 +0300

    refactor DropdownMenu and update preview

--- composeApp/src/commonMain/kotlin/com/cairosquad/evolvefit/ui/screen/createExercise/content/DropdownMenu.kt
+++ composeApp/src/commonMain/kotlin/com/cairosquad/evolvefit/ui/screen/createExercise/content/DropdownMenu.kt
@@ -2,12 +2,16 @@ package com.cairosquad.evolvefit.ui.screen.createExercise.content
 
 import androidx.compose.foundation.background
 import androidx.compose.foundation.clickable
+import androidx.compose.foundation.interaction.MutableInteractionSource
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material3.DropdownMenu
-import androidx.compose.material3.DropdownMenuItem
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.getValue
@@ -25,66 +29,90 @@ import org.jetbrains.compose.ui.tooling.preview.Preview
 @Composable
 fun CustomDropdownMenu(
     items: List<String>,
-    selectedItem: String?,
     expanded: Boolean,
     onItemSelected: (String) -> Unit,
     onDismissRequest: () -> Unit,
     isChecked: (String) -> Boolean
 ) {
     DropdownMenu(
+        modifier = Modifier
+            .fillMaxWidth()
+            .background(
+                Theme.color.surfaces.surfaceContainer,
+                shape = RoundedCornerShape(8.dp)
+            )
+            .padding(horizontal = 16.dp),
         expanded = expanded,
-        onDismissRequest = onDismissRequest,
-        modifier = Modifier.background(Theme.color.surfaces.surface)
+        onDismissRequest = onDismissRequest
     ) {
         items.forEach { item ->
-            DropdownMenuItem(
-                text = {
-                    Row(
-                        modifier = Modifier
-                            .fillMaxWidth(),
-                        verticalAlignment = Alignment.CenterVertically,
+            Row(
+                modifier = Modifier
+                    .fillMaxWidth()
+                    .padding(vertical = 8.dp)
+                    .clickable(
+                        interactionSource = remember { MutableInteractionSource() },
+                        indication = null
                     ) {
-                        Text(
-                            text = item,
-                            style = Theme.textStyle.label.smallRegular12,
-                            color = Theme.color.surfaces.onSurfaceVariant
-                        )
-                        Spacer(modifier = Modifier.weight(1f))
-                        CustomTick(
-                            isChecked = item == selectedItem,
-                            onCheckedChange = { onItemSelected(item) }
-                        )
-                    }
-                },
-                onClick = { onItemSelected(item) }
-            )
+                        onItemSelected(item)
+                    },
+                verticalAlignment = Alignment.CenterVertically
+            ) {
+                Text(
+                    text = item,
+                    style = Theme.textStyle.body.mediumMedium14,
+                    color = Theme.color.surfaces.onSurfaceContainer
+                )
+                Spacer(modifier = Modifier.weight(1f))
+                CustomTick(
+                    modifier = Modifier.padding(vertical = 8.dp),
+                    isChecked = isChecked(item),
+                    onCheckedChange = { onItemSelected(item) }
+                )
+            }
         }
     }
 }
-
 
 @Preview
 @Composable
 private fun CustomDropdownMenuPrev() {
     AppTheme(isDarkTheme = true) {
-        var selectedItem by remember { mutableStateOf<String?>(null) }
-        var expanded by remember { mutableStateOf(true) }
-
-        Text(
+        Box(
             modifier = Modifier
-                .clickable { expanded = !expanded }
+                .fillMaxSize()
+                .background(color = Theme.color.surfaces.surface)
                 .padding(16.dp),
-            text = "Open Dropdown",
-            style = Theme.textStyle.label.smallRegular12,
-            color = Theme.color.surfaces.onSurfaceVariant
-        )
-        CustomDropdownMenu(
-            items = listOf("Upper Body", "Lower Body", "Full Body"),
-            selectedItem = selectedItem,
-            onItemSelected = { selectedItem = it },
-            expanded = expanded,
-            onDismissRequest = { expanded = false },
-            isChecked = { it == selectedItem }
-        )
+            contentAlignment = Alignment.Center
+        ) {
+            var selectedItem by remember { mutableStateOf<String?>(null) }
+            var expanded by remember { mutableStateOf(true) }
+
+            Column {
+                Text(
+                    modifier = Modifier
+                        .clickable { expanded = !expanded }
+                        .padding(bottom = 8.dp),
+                    text = "Open Dropdown",
+                    style = Theme.textStyle.label.smallRegular12,
+                    color = Theme.color.surfaces.onSurfaceContainer
+                )
+                CustomDropdownMenu(
+                    items = listOf(
+                        "Upper Body",
+                        "Lower Body",
+                        "Full Body",
+                        "Upper Body1",
+                        "Lower Body1",
+                        "Full Body1"
+                    ),
+                    onItemSelected = { selectedItem = it },
+                    expanded = expanded,
+                    onDismissRequest = { expanded = false },
+                    isChecked = { it == selectedItem }
+                )
+            }
+        }
     }
 }
+
