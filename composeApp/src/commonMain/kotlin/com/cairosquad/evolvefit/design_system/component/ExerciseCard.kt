fa6c513f2f3b7d34c44383426d0932ff7efd4578
Shahd-Hatem <Shahdhatim863@gmail.com>
2025-08-11 21:13:42 +0300

    add tick to ExerciseCard

--- composeApp/src/commonMain/kotlin/com/cairosquad/evolvefit/design_system/component/ExerciseCard.kt
+++ composeApp/src/commonMain/kotlin/com/cairosquad/evolvefit/design_system/component/ExerciseCard.kt
@@ -10,6 +10,10 @@ import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.clip
@@ -26,13 +30,24 @@ import org.jetbrains.compose.ui.tooling.preview.Preview
 @Composable
 fun ExerciseCard(
     title: String,
-    time: String,
     modifier: Modifier = Modifier,
-    unit: String = stringResource(Res.string.seconds),
-    model: String
+    model: String,
+    timeInSeconds: Int? = null,
+    reps: Int? = null,
+    sets: Int? = null,
+    isChecked: Boolean,
+    onCheckedChange: (Boolean) -> Unit
 ) {
+    val unitText = when {
+        timeInSeconds != null -> "$timeInSeconds ${stringResource(Res.string.seconds)}"
+        reps != null && sets != null -> "$sets X$reps"
+        else -> ""
+    }
+
     Row(
-        modifier = modifier.fillMaxWidth(),
+        modifier = modifier
+            .fillMaxWidth()
+            .padding(12.dp),
         verticalAlignment = Alignment.CenterVertically
     ) {
         NetworkImage(
@@ -42,8 +57,10 @@ fun ExerciseCard(
             model = model,
             contentDescription = stringResource(Res.string.exercise_image),
         )
+
         Column(
-            modifier = Modifier.weight(1f)
+            modifier = Modifier
+                .weight(1f)
                 .padding(start = 12.dp),
         ) {
             Text(
@@ -52,25 +69,48 @@ fun ExerciseCard(
                 color = Theme.color.surfaces.onSurfaceContainer
             )
             Text(
-                text = "$time $unit",
+                text = unitText,
                 style = Theme.textStyle.label.smallRegular12,
                 color = Theme.color.surfaces.onSurfaceVariant,
                 modifier = Modifier.padding(top = 12.dp)
             )
         }
+
+        CustomTick(
+            isChecked = isChecked,
+            onCheckedChange = onCheckedChange
+        )
     }
 }
 
 @Preview
 @Composable
 private fun ExerciseCardPreview() {
+    var isChecked by remember { mutableStateOf(false) }
+
     AppTheme(isDarkTheme = true) {
-        Box(
+        Column(
             Modifier
                 .background(Theme.color.surfaces.surface)
                 .padding(16.dp)
         ) {
-            ExerciseCard(title = "Bodyweight Squats", time = "30", model = "")
+            ExerciseCard(
+                title = "Jumping Jacks",
+                timeInSeconds = 30,
+                model = "",
+                isChecked = isChecked,
+                onCheckedChange = { isChecked = it }
+            )
+
+            ExerciseCard(
+                title = "Push-ups",
+                reps = 10,
+                sets = 3,
+                model = "",
+                isChecked = !isChecked,
+                onCheckedChange = { isChecked = !isChecked }
+            )
         }
     }
 }
+
