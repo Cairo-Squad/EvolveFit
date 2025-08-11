e6d8e30ddbc2bd478d2579267de6502f8fd1ed54
Youssef Mohamed <youssef.mu.saber@gmail.com>
2025-08-11 16:48:50 +0300

    feature: implemented the base logic structure for report repository and use case.

--- composeApp/src/commonMain/kotlin/com/cairosquad/evolvefit/di/repositoryModule.kt
+++ composeApp/src/commonMain/kotlin/com/cairosquad/evolvefit/di/repositoryModule.kt
@@ -1,10 +1,12 @@
 package com.cairosquad.evolvefit.di
 
 import com.cairosquad.evolvefit.domain.AuthRepository
+import com.cairosquad.evolvefit.domain.ReportRepository
 import com.cairosquad.evolvefit.local.AuthPreferences
 import com.cairosquad.evolvefit.remote.auth.AuthRemoteDataSourceImp
 import com.cairosquad.evolvefit.remote.provideHttpClient
 import com.cairosquad.evolvefit.repository.AuthRepositoryImpl
+import com.cairosquad.evolvefit.repository.FakeReportRepositoryImpl
 import com.cairosquad.evolvefit.repository.remote.AuthRemoteDataSource
 import io.ktor.client.plugins.auth.providers.BearerTokens
 import org.koin.dsl.module
@@ -21,5 +23,6 @@ val repositoryModule = module {
         )
     }
     single<AuthRemoteDataSource> { AuthRemoteDataSourceImp(get()) }
-    single<AuthRepository> { AuthRepositoryImpl(get(),get()) }
+    single<AuthRepository> { AuthRepositoryImpl(get(), get()) }
+    single<ReportRepository> { FakeReportRepositoryImpl() }
 }
--- composeApp/src/commonMain/kotlin/com/cairosquad/evolvefit/di/useCaseModule.kt
+++ composeApp/src/commonMain/kotlin/com/cairosquad/evolvefit/di/useCaseModule.kt
@@ -1,8 +1,10 @@
 package com.cairosquad.evolvefit.di
 
 import com.cairosquad.evolvefit.domain.usecase.authentication.AuthUseCase
+import com.cairosquad.evolvefit.domain.usecase.report.ReportUseCase
 import org.koin.dsl.module
 
 val useCaseModule = module {
     factory { AuthUseCase(get()) }
+    factory { ReportUseCase(get()) }
 }
