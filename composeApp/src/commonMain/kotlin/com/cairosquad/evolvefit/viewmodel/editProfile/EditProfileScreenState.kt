225488737928a7dfd3102a4c83a614317dfaefbe
gehadepada <gehadepada@gmail.com>
2025-08-11 17:12:56 +0300

    feat:implement EditProfileScreenState

--- composeApp/src/commonMain/kotlin/com/cairosquad/evolvefit/viewmodel/editProfile/EditProfileScreenState.kt
+++ composeApp/src/commonMain/kotlin/com/cairosquad/evolvefit/viewmodel/editProfile/EditProfileScreenState.kt
@@ -1,4 +1,16 @@
 package com.cairosquad.evolvefit.viewmodel.editProfile
 
-class EditProfileScreenState {
-}
+class EditProfileScreenState (
+    val fullName : String="",
+    val email : String ="",
+    val dateOfBirth : String ="",
+    val gender: String = "",
+    val height: String = "",
+    val weight: String = "",
+    val mainGoal: String = "",
+    val level: String = "",
+    val tools: String = "",
+    val workoutDays: List<String> = emptyList(),
+    val profileImageUri: String? = null,
+    val showBottomSheet: Boolean = false,
+    )
