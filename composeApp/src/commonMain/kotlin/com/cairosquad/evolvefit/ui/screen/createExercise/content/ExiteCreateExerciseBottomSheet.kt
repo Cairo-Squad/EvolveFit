881ed5c32a15b1fa2dc45dee0a889272d83a5d0f
Hager Abobakr <hagarabobakr1@gmail.com>
2025-08-11 14:39:30 +0300

    add exit create exercise bottom sheet
    This commit introduces a new bottom sheet component `ExiteCreateExerciseBottomSheet` used when a user attempts to exit the exercise creation screen without saving.
    
    - Added `ExiteCreateExerciseBottomSheet.kt` with the Composable function for the bottom sheet UI.
    - Included preview function `ExiteCreateExerciseBottomSheetPreview` for easier UI development and testing.
    - Added new drawable resources `im_save_dark.png` and `im_save_light.png` for the save icon in the bottom sheet, supporting both dark and light themes.

---
+++ composeApp/src/commonMain/kotlin/com/cairosquad/evolvefit/ui/screen/createExercise/content/ExiteCreateExerciseBottomSheet.kt
@@ -0,0 +1,124 @@
+package com.cairosquad.evolvefit.ui.screen.createExercise.content
+
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.clickable
+import androidx.compose.foundation.isSystemInDarkTheme
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.unit.dp
+import com.cairosquad.evolvefit.design_system.component.BottomSheet
+import com.cairosquad.evolvefit.design_system.component.PrimaryButton
+import com.cairosquad.evolvefit.design_system.theme.AppTheme
+import com.cairosquad.evolvefit.design_system.theme.Theme
+import evolvefit.composeapp.generated.resources.Res
+import evolvefit.composeapp.generated.resources.im_save_dark
+import evolvefit.composeapp.generated.resources.im_save_light
+import org.jetbrains.compose.resources.painterResource
+import org.jetbrains.compose.ui.tooling.preview.Preview
+
+@Composable
+fun ExiteCreateExerciseBottomSheet(
+    isVisible: Boolean = false,
+    onDismiss: () -> Unit = {},
+    onCancelClicked: () -> Unit = {},
+    onExitWithoutSavingClicked: () -> Unit = {}
+) {
+    val saveImage =
+        if (isSystemInDarkTheme()) painterResource(Res.drawable.im_save_dark)
+        else painterResource(Res.drawable.im_save_light)
+    BottomSheet(
+        isVisible = isVisible,
+        onDismiss = onDismiss,
+    ) {
+        Column(
+            modifier = Modifier.padding(16.dp),
+            horizontalAlignment = Alignment.CenterHorizontally
+        ) {
+            Image(
+                modifier = Modifier.padding(bottom = 16.dp)
+                    .padding(horizontal = 98.dp),
+                painter = saveImage,
+                contentDescription = "save image"
+            )
+            Text(
+                modifier = Modifier
+                    .padding(bottom = 8.dp),
+                text = "Do you want to exit?",
+                style = Theme.textStyle.title.mediumMedium16,
+                color = Theme.color.surfaces.onSurface
+            )
+            Text(
+                modifier = Modifier
+                    .padding(bottom = 24.dp),
+                text = "You haven’t saved your workout yet. All progress might be lost.",
+                style = Theme.textStyle.label.smallRegular12,
+                color = Theme.color.surfaces.onSurfaceVariant
+            )
+            PrimaryButton(
+                modifier = Modifier.padding(top = 12.dp),
+                text = "Cancel",
+                onClick = onCancelClicked
+            )
+            PrimaryButton(
+                text = "Exit Without Saving",
+                enabledContainerColor = Color.Transparent,
+                enabledTextColor = Theme.color.surfaces.onSurfaceContainer,
+                onClick = onExitWithoutSavingClicked
+            )
+        }
+
+    }
+}
+
+
+@Preview()
+@Composable
+fun ExiteCreateExerciseBottomSheetPreview() {
+    val saveImage =
+        if (isSystemInDarkTheme()) painterResource(Res.drawable.im_save_dark)
+        else painterResource(Res.drawable.im_save_light)
+
+    AppTheme(isDarkTheme = false) {
+        Column(
+            modifier = Modifier.padding(16.dp),
+            horizontalAlignment = Alignment.CenterHorizontally
+        ) {
+            Image(
+                modifier = Modifier.padding(bottom = 16.dp)
+                    .padding(horizontal = 98.dp),
+                painter = saveImage,
+                contentDescription = "save image"
+            )
+            Text(
+                modifier = Modifier
+                    .padding(bottom = 8.dp),
+                text = "Do you want to exit?",
+                style = Theme.textStyle.title.mediumMedium16,
+                color = Theme.color.surfaces.onSurface
+            )
+            Text(
+                modifier = Modifier
+                    .padding(bottom = 24.dp),
+                text = "You haven’t saved your workout yet. All progress might be lost.",
+                style = Theme.textStyle.label.smallRegular12,
+                color = Theme.color.surfaces.onSurfaceVariant
+            )
+            PrimaryButton(
+                modifier = Modifier.padding(top = 12.dp),
+                text = "Cancel",
+                onClick = {}
+            )
+            PrimaryButton(
+                text = "Exit Without Saving",
+                enabledContainerColor = Color.Transparent,
+                enabledTextColor = Theme.color.surfaces.onSurfaceContainer,
+                onClick = {}
+            )
+        }
+    }
+}
