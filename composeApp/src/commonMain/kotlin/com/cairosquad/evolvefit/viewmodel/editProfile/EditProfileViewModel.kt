5a3886c460e8812e1437a48fd8d0b4f135b0a597
gehadepada <gehadepada@gmail.com>
2025-08-11 17:47:44 +0300

    feat:put base structure for EditProfileViewModel

--- composeApp/src/commonMain/kotlin/com/cairosquad/evolvefit/viewmodel/editProfile/EditProfileViewModel.kt
+++ composeApp/src/commonMain/kotlin/com/cairosquad/evolvefit/viewmodel/editProfile/EditProfileViewModel.kt
@@ -1,4 +1,34 @@
 package com.cairosquad.evolvefit.viewmodel.editProfile
 
-class EditProfileViewModel {
-}
+import com.cairosquad.evolvefit.viewmodel.base.BaseViewModel
+
+class EditProfileViewModel() :BaseViewModel<EditProfileScreenState, EditProfileEffect>(
+    EditProfileScreenState()) , EditProfileInteractionListener
+ {
+     override fun onBackClicked() {
+         TODO("Not yet implemented")
+     }
+
+     override fun onEditInfoClicked(infoType: InfoType) {
+         TODO("Not yet implemented")
+     }
+
+     override fun onSaveChangesClicked() {
+         TODO("Not yet implemented")
+     }
+
+     override fun onProfileImageClicked() {
+         TODO("Not yet implemented")
+     }
+
+     override fun onBottomSheetDismissed() {
+         TODO("Not yet implemented")
+     }
+
+     override fun onInfoValueSelected(
+         infoType: InfoType,
+         value: String
+     ) {
+         TODO("Not yet implemented")
+     }
+ }
