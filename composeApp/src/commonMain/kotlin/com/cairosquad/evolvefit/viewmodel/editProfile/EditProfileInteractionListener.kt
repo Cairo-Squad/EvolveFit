5e1736375f69681d2a81e503e23709a5285db98a
gehadepada <gehadepada@gmail.com>
2025-08-11 17:05:55 +0300

    feat:implement EditProfileInteractionListener

--- composeApp/src/commonMain/kotlin/com/cairosquad/evolvefit/viewmodel/editProfile/EditProfileInteractionListener.kt
+++ composeApp/src/commonMain/kotlin/com/cairosquad/evolvefit/viewmodel/editProfile/EditProfileInteractionListener.kt
@@ -1,4 +1,10 @@
 package com.cairosquad.evolvefit.viewmodel.editProfile
 
 interface EditProfileInteractionListener {
+    fun onBackClicked()
+    fun onEditInfoClicked(infoType : InfoType)
+    fun onSaveChangesClicked()
+    fun onProfileImageClicked()
+    fun onBottomSheetDismissed()
+    fun onInfoValueSelected(infoType: InfoType, value: String)
 }
---
+++ composeApp/src/commonMain/kotlin/com/cairosquad/evolvefit/viewmodel/editProfile/InfoType.kt
@@ -0,0 +1,15 @@
+package com.cairosquad.evolvefit.viewmodel.editProfile
+
+enum class InfoType {
+    FULL_NAME,
+    EMAIL,
+    DATE_OF_BIRTH,
+    GENDER,
+    HEIGHT,
+    WEIGHT,
+    MAIN_GOAL,
+    LEVEL,
+    TOOLS,
+    WORKOUT_DAYS,
+    PROFILE_IMAGE
+}
