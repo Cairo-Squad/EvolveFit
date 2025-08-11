7bba5dcc5bb950ef1e75baf79d5be6a85d6217d4
Shahd-Hatem <Shahdhatim863@gmail.com>
2025-08-11 21:10:56 +0300

    add work out to useCaseModule

--- composeApp/src/commonMain/kotlin/com/cairosquad/evolvefit/di/useCaseModule.kt
+++ composeApp/src/commonMain/kotlin/com/cairosquad/evolvefit/di/useCaseModule.kt
@@ -1,8 +1,12 @@
 package com.cairosquad.evolvefit.di
 
+import com.cairosquad.evolvefit.domain.usecase.CreateWorkoutUseCase
 import com.cairosquad.evolvefit.domain.usecase.authentication.AuthUseCase
 import org.koin.dsl.module
 
 val useCaseModule = module {
+    factory { AuthUseCase(get()) }
+    factory { CreateWorkoutUseCase(get()) }
+    //factory { GetEquipmentsUseCase(get()) }
     factory { AuthUseCase(get()) }
 }
