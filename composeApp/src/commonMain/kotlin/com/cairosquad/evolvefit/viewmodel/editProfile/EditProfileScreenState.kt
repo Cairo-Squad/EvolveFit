49259e34d83ab4efe65a399b6de0e13ee22103ab
gehadepada <gehadepada@gmail.com>
2025-08-12 12:19:21 +0300

    refactor:handle EditProfileScreenState

--- composeApp/src/commonMain/kotlin/com/cairosquad/evolvefit/viewmodel/editProfile/EditProfileScreenState.kt
+++ composeApp/src/commonMain/kotlin/com/cairosquad/evolvefit/viewmodel/editProfile/EditProfileScreenState.kt
@@ -1,16 +1,21 @@
 package com.cairosquad.evolvefit.viewmodel.editProfile
 
-class EditProfileScreenState (
-    val fullName : String="",
-    val email : String ="",
-    val dateOfBirth : String ="",
-    val gender: String = "",
-    val height: String = "",
-    val weight: String = "",
-    val mainGoal: String = "",
-    val level: String = "",
-    val tools: String = "",
-    val workoutDays: List<String> = emptyList(),
-    val profileImageUri: String? = null,
+data class EditProfileScreenState(
+    val user : UserUiState= UserUiState(),
     val showBottomSheet: Boolean = false,
-    )
+) {
+    data class UserUiState(
+        val fullName: String = "",
+        val email: String = "",
+        val dateOfBirth: String = "",
+        val gender: String = "",
+        val height: String = "",
+        val weight: String = "",
+        val mainGoal: String = "",
+        val tools: String = "",
+        val workoutDays: List<String> = emptyList(),
+        val profileImageUri: String? = null,
+
+        )
+}
+
