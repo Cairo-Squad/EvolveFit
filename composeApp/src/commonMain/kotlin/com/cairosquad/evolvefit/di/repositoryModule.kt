53379469709603129f90814e59152a16f539d143
Shahd-Hatem <Shahdhatim863@gmail.com>
2025-08-13 15:15:59 +0300

    solve register problem

--- composeApp/src/commonMain/kotlin/com/cairosquad/evolvefit/di/repositoryModule.kt
+++ composeApp/src/commonMain/kotlin/com/cairosquad/evolvefit/di/repositoryModule.kt
@@ -1,10 +1,12 @@
 package com.cairosquad.evolvefit.di
 
 import com.cairosquad.evolvefit.domain.repository.AuthenticationRepository
+import com.cairosquad.evolvefit.domain.repository.EquipmentRepository
 import com.cairosquad.evolvefit.domain.repository.ReportRepository
 import com.cairosquad.evolvefit.domain.repository.WorkoutRepository
 import com.cairosquad.evolvefit.repository.FakeReportRepositoryImpl
 import com.cairosquad.evolvefit.repository.authentication.AuthenticationRepositoryImpl
+import com.cairosquad.evolvefit.repository.equipment.EquipmentRepositoryImpl
 import com.cairosquad.evolvefit.repository.workout.WorkoutRepositoryImpl
 import org.koin.core.module.dsl.singleOf
 import org.koin.dsl.bind
@@ -14,4 +16,5 @@ val repositoryModule = module {
     singleOf(::AuthenticationRepositoryImpl) bind AuthenticationRepository::class
     singleOf(::WorkoutRepositoryImpl) bind WorkoutRepository::class
     singleOf(::FakeReportRepositoryImpl) bind ReportRepository::class
+    singleOf(::EquipmentRepositoryImpl) bind EquipmentRepository::class
 }
