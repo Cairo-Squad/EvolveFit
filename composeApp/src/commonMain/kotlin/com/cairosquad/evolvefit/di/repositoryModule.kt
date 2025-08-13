dedcc057f3e8ce4ed9b80010d9141f1e382d3b43
gehadepada <gehadepada@gmail.com>
2025-08-13 18:07:37 +0300

    fix:fix crash when navigate to editProfile screen

--- composeApp/src/commonMain/kotlin/com/cairosquad/evolvefit/di/repositoryModule.kt
+++ composeApp/src/commonMain/kotlin/com/cairosquad/evolvefit/di/repositoryModule.kt
@@ -2,12 +2,14 @@ package com.cairosquad.evolvefit.di
 
 import com.cairosquad.evolvefit.domain.repository.AuthenticationRepository
 import com.cairosquad.evolvefit.domain.repository.EquipmentRepository
+import com.cairosquad.evolvefit.domain.repository.ProfileRepository
 import com.cairosquad.evolvefit.domain.repository.ReportRepository
 import com.cairosquad.evolvefit.domain.repository.WorkoutRepository
 import com.cairosquad.evolvefit.repository.FakeReportRepositoryImpl
 import com.cairosquad.evolvefit.repository.authentication.AuthenticationRepositoryImpl
 import com.cairosquad.evolvefit.repository.equipment.EquipmentRepositoryImpl
 import com.cairosquad.evolvefit.repository.workout.WorkoutRepositoryImpl
+import com.cairosquad.evolvefit.repository.profile.ProfileRepositoryImpl
 import org.koin.core.module.dsl.singleOf
 import org.koin.dsl.bind
 import org.koin.dsl.module
@@ -17,4 +19,5 @@ val repositoryModule = module {
     singleOf(::WorkoutRepositoryImpl) bind WorkoutRepository::class
     singleOf(::FakeReportRepositoryImpl) bind ReportRepository::class
     singleOf(::EquipmentRepositoryImpl) bind EquipmentRepository::class
+    singleOf(::ProfileRepositoryImpl) bind ProfileRepository::class
 }
