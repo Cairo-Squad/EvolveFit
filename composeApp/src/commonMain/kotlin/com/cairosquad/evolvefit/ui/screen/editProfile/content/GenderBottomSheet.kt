595855e90795229d0b6b19167cfc0391a036dd39
gehadepada <gehadepada@gmail.com>
2025-08-14 00:29:39 +0300

    feat :finish GenderBottomSheet

--- composeApp/src/commonMain/kotlin/com/cairosquad/evolvefit/ui/screen/editProfile/content/GenderBottomSheet.kt
+++ composeApp/src/commonMain/kotlin/com/cairosquad/evolvefit/ui/screen/editProfile/content/GenderBottomSheet.kt
@@ -1,4 +1,85 @@
 package com.cairosquad.evolvefit.ui.screen.editProfile.content
 
-class GenderBottomSheet {
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.unit.dp
+import com.cairosquad.evolvefit.design_system.component.BottomSheet
+import com.cairosquad.evolvefit.design_system.component.CheckboxItem
+import com.cairosquad.evolvefit.design_system.component.PrimaryButton
+import com.cairosquad.evolvefit.design_system.theme.Theme
+import com.cairosquad.evolvefit.ui.screen.register.content.RegisterHeader
+import com.cairosquad.evolvefit.viewmodel.register.RegisterScreenState
+import evolvefit.composeapp.generated.resources.Res
+import evolvefit.composeapp.generated.resources.female
+import evolvefit.composeapp.generated.resources.male
+import evolvefit.composeapp.generated.resources.select_gender_description
+import evolvefit.composeapp.generated.resources.select_gender_title
+import evolvefit.composeapp.generated.resources.save
+import org.jetbrains.compose.resources.stringResource
+
+@Composable
+fun GenderBottomSheet(
+    selectedGender: String,
+    isGenderBottomSheetOpen: Boolean=true,
+    onGenderBottomSheetDismiss: () -> Unit,
+    onGenderChange: (String) -> Unit,
+    modifier: Modifier = Modifier
+) {
+    BottomSheet(
+        isVisible = isGenderBottomSheetOpen,
+        onDismiss = onGenderBottomSheetDismiss,
+        modifier = modifier.fillMaxWidth()
+    ) {
+        GenderBottomSheetContent(
+            selectedGender = selectedGender,
+            onGenderChange = onGenderChange,
+            onGenderBottomSheetDismiss = onGenderBottomSheetDismiss,
+            modifier = Modifier.padding(16.dp)
+        )
+    }
+}
+
+@Composable
+fun GenderBottomSheetContent(
+    selectedGender:String ,
+    onGenderChange: (String) -> Unit,
+    onGenderBottomSheetDismiss: () -> Unit,
+    modifier: Modifier = Modifier
+) {
+    Column(
+        modifier = modifier.fillMaxWidth()
+    ) {
+        RegisterHeader(
+            modifier = Modifier.padding(bottom = 16.dp),
+            title = stringResource(Res.string.select_gender_title),
+            titleStyle = Theme.textStyle.label.mediumMedium16
+        )
+
+        Column(
+            modifier = Modifier
+                .fillMaxWidth()
+                .padding(bottom = 40.dp),
+            verticalArrangement = Arrangement.spacedBy(12.dp)
+        ) {
+            RegisterScreenState.Gender.entries.forEach { gender ->
+                CheckboxItem(
+                    text = when (gender) {
+                        RegisterScreenState.Gender.Male -> stringResource(Res.string.male)
+                        RegisterScreenState.Gender.Female -> stringResource(Res.string.female)
+                    },
+                    isChecked = selectedGender== gender.name,
+                    onCheckedChange = { onGenderChange(gender.name) },
+                )
+            }
+        }
+
+        PrimaryButton(
+            text = stringResource(Res.string.save),
+            onClick = onGenderBottomSheetDismiss
+        )
+    }
 }
