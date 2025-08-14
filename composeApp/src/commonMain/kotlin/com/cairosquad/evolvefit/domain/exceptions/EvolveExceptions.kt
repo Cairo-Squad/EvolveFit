a00c31936943a4ec7e634ac211cd16c285795e5c
Elsayed Magdy <saikoo1239@gmail.com>
2025-08-14 19:03:21 +0300

    refactor : add input exceptions

--- composeApp/src/commonMain/kotlin/com/cairosquad/evolvefit/domain/exceptions/EvolveExceptions.kt
+++ composeApp/src/commonMain/kotlin/com/cairosquad/evolvefit/domain/exceptions/EvolveExceptions.kt
@@ -41,3 +41,28 @@ class DataParsingException(message: String = "Error parsing data", cause: Throwa
 
 class UnknownException(message: String = "Unknown error", cause: Throwable? = null) :
     EvolveException(message, cause)
+
+open class NutritionException(
+    message: String = "Nutrition error",
+    cause: Throwable? = null
+) : EvolveException(message, cause)
+
+class ExceededCaloriesException(
+    message: String = "Not enough remaining calories",
+    cause: Throwable? = null
+) : NutritionException(message, cause)
+
+class ExceededWaterLimitException(
+    message: String = "Not enough remaining water allowance",
+    cause: Throwable? = null
+) : NutritionException(message, cause)
+
+class MealNotFoundException(
+    message: String = "Meal not found",
+    cause: Throwable? = null
+) : NutritionException(message, cause)
+
+class InvalidNumberFormatException(
+    message: String = "Only numbers and decimal point are allowed",
+    cause: Throwable? = null
+) : EvolveException(message, cause)
