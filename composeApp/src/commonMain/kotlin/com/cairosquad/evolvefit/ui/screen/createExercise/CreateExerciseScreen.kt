955d61e0bfff4a0a987891118c14605b82e10e41
Hager Abobakr <hagarabobakr1@gmail.com>
2025-08-14 18:53:12 +0300

    refactor CreateExerciseScreen
    This commit refactors the CreateExerciseScreen composable:
    - Replaced `statusBarsPadding()` and `navigationBarsPadding()` with `windowInsetsPadding(WindowInsets.systemBars)` for better handling of system insets.
    - Moved the `ImagePicker` composable out of the `LazyColumn` item and into the parent `Box` to ensure it overlays other content when visible.
    - Removed the `isEnabled` parameter from the `PrimaryButton` as it was always true.
    - Made a minor adjustment to the `onClick` lambda for the save button.

--- composeApp/src/commonMain/kotlin/com/cairosquad/evolvefit/ui/screen/createExercise/CreateExerciseScreen.kt
+++ composeApp/src/commonMain/kotlin/com/cairosquad/evolvefit/ui/screen/createExercise/CreateExerciseScreen.kt
@@ -10,12 +10,15 @@ import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.WindowInsets
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.navigationBarsPadding
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.statusBarsPadding
+import androidx.compose.foundation.layout.systemBars
+import androidx.compose.foundation.layout.windowInsetsPadding
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.text.KeyboardOptions
 import androidx.compose.material3.Text
@@ -112,6 +115,7 @@ fun CreateExerciseScreenContent(
     } else {
         state.selectedFocusAreasText
     }
+
     val durationTitleColor = if (state.isDurationChecked)
         Theme.color.surfaces.onSurfaceContainer
     else
@@ -132,206 +136,214 @@ fun CreateExerciseScreenContent(
     else
         Theme.color.surfaces.onSurfaceVariant
 
-    Column(
+
+    Box(
         modifier = Modifier
             .fillMaxSize()
             .background(color = Theme.color.surfaces.surface)
-            .padding(horizontal = 16.dp)
-            .statusBarsPadding()
-            .navigationBarsPadding(),
-        horizontalAlignment = Alignment.CenterHorizontally,
     ) {
-        CustomAppBar(
-            modifier = Modifier.padding(bottom = 16.dp),
-            title = "",
-            header = {
-                ActionIconButton(
-                    icon = painterResource(Res.drawable.ic_cancle),
-                    contentDescription = stringResource(Res.string.cancel),
-                    tint = Theme.color.surfaces.onSurface,
-                    onClick = listener::onExitClicked
-                )
-            }
-        )
-        LazyColumn(
+        Column(
             modifier = Modifier
-                .weight(1f)
-                .fillMaxWidth(),
-            horizontalAlignment = Alignment.CenterHorizontally
+                .fillMaxSize()
+                .padding(horizontal = 16.dp)
+                .windowInsetsPadding(WindowInsets.systemBars),
+            horizontalAlignment = Alignment.CenterHorizontally,
         ) {
-            item {
-                RegisterHeader(
-                    modifier = Modifier.padding(bottom = 24.dp),
-                    title = stringResource(Res.string.create_exercise_title),
-                    description = stringResource(Res.string.create_exercise_description)
-                )
-            }
-            item {
-                Box(
-                    modifier = Modifier
-                        .padding(bottom = 8.dp)
-                        .padding(horizontal = 105.dp)
-                        .clickable(onClick = listener::onImagePickerClicked),
-                    contentAlignment = Alignment.Center
-                ) {
-                    if (state.isImagePickerOpen) {
-                        ImagePicker(
-                            onImageRetrieved = listener::onImageRetrieved,
-                            onImagePickerDismiss = listener::onImagePickerDismiss
-                        )
-                    }
-
-                    if (selectedImage != null) {
-                        UiImageDisplayer(
-                            image = selectedImage,
-                            contentDescription = stringResource(Res.string.exercise_image),
-                            defaultImageSize = 64.dp
-                        )
-                    } else {
-                        Image(
-                            painter = uploadExercisesImg!!,
-                            contentDescription = stringResource(Res.string.upload_image)
-                        )
-                    }
-                }
-            }
-
-            item {
-                Text(
-                    modifier = Modifier
-                        .padding(bottom = 24.dp)
-                        .clickable(onClick = listener::onImagePickerClicked),
-                    text = stringResource(Res.string.upload_image),
-                    style = Theme.textStyle.label.smallRegular12,
-                    color = Theme.color.surfaces.onSurfaceVariant
-                )
-            }
-
-            item {
-                InputField(
-                    modifier = Modifier.padding(bottom = 12.dp),
-                    value = state.name,
-                    onValueChange = listener::onNameChanged,
-                    placeholder = stringResource(Res.string.enter_exercise_name)
-                )
-            }
-            item {
-                RowWithIcon(
-                    modifier = Modifier
-                        .padding(bottom = 12.dp),
-                    text = selectedFocusArea,
-                    textColor = selectedFocusAreaTextColor,
-                    isIconClicked = state.isFocusAreaExpanded,
-                    onIconClicked = listener::onFocusAreaIconClicked
-                )
-                AnimatedVisibility(visible = state.isFocusAreaExpanded) {
-                    Column {
-                        CustomDropdownMenu(
-                            items = state.focusAreaNames,
-                            onItemSelected = listener::onFocusAreaNameSelected,
-                            isChecked = state::isFocusAreaSelected
-                        )
-                        Spacer(modifier = Modifier.height(8.dp))
-                    }
-                }
-            }
-            item {
-                Row(
-                    modifier = Modifier
-                        .fillMaxWidth()
-                        .padding(bottom = 12.dp),
-                    horizontalArrangement = Arrangement.spacedBy(8.dp)
-                ) {
-                    CheckboxItem(
-                        modifier = Modifier.weight(1f),
-                        text = stringResource(Res.string.add_duration),
-                        titleColor = durationTitleColor,
-                        isChecked = state.isDurationChecked,
-                        onCheckedChange = {
-                            if (it) listener.onMeasurementTypeSelected(
-                                CreateExerciseState.MeasurementType.DURATION
-                            )
-                        },
-                        style = CheckboxStyle.Tick
-                    )
-                    CheckboxItem(
-                        modifier = Modifier.weight(1f),
-                        text = stringResource(Res.string.add_reps),
-                        titleColor = repsTitleColor,
-                        isChecked = state.isRepsChecked,
-                        onCheckedChange = {
-                            if (it) listener.onMeasurementTypeSelected(
-                                CreateExerciseState.MeasurementType.REPS
-                            )
-                        },
-                        style = CheckboxStyle.Tick
+            CustomAppBar(
+                modifier = Modifier.padding(bottom = 16.dp),
+                title = "",
+                header = {
+                    ActionIconButton(
+                        icon = painterResource(Res.drawable.ic_cancle),
+                        contentDescription = stringResource(Res.string.cancel),
+                        tint = Theme.color.surfaces.onSurface,
+                        onClick = listener::onExitClicked
                     )
                 }
-            }
-            item {
-                AnimatedVisibility(
-                    visible = state.isDurationChecked || state.isRepsChecked,
-                ) {
+            )
+
+            LazyColumn(
+                modifier = Modifier
+                    .weight(1f)
+                    .fillMaxWidth(),
+                horizontalAlignment = Alignment.CenterHorizontally
+            ) {
+                item {
+                    RegisterHeader(
+                        modifier = Modifier.padding(bottom = 24.dp),
+                        title = stringResource(Res.string.create_exercise_title),
+                        description = stringResource(Res.string.create_exercise_description)
+                    )
+                }
+
+                item {
+                    Box(
+                        modifier = Modifier
+                            .padding(bottom = 8.dp)
+                            .padding(horizontal = 105.dp)
+                            .clickable(onClick = listener::onImagePickerClicked),
+                        contentAlignment = Alignment.Center
+                    ) {
+                        if (selectedImage != null) {
+                            UiImageDisplayer(
+                                image = selectedImage,
+                                contentDescription = stringResource(Res.string.exercise_image),
+                                defaultImageSize = 64.dp
+                            )
+                        } else {
+                            Image(
+                                painter = uploadExercisesImg!!,
+                                contentDescription = stringResource(Res.string.upload_image)
+                            )
+                        }
+                    }
+                }
+
+                item {
+                    Text(
+                        modifier = Modifier
+                            .padding(bottom = 24.dp)
+                            .clickable(onClick = listener::onImagePickerClicked),
+                        text = stringResource(Res.string.upload_image),
+                        style = Theme.textStyle.label.smallRegular12,
+                        color = Theme.color.surfaces.onSurfaceVariant
+                    )
+                }
+
+                item {
                     InputField(
                         modifier = Modifier.padding(bottom = 12.dp),
-                        value = state.measurementInputValue,
-                        onValueChange = {
-                            listener.onMeasurementValueChanged(it)
-                        },
-                        placeholder = when (state.measurementType) {
-                            CreateExerciseState.MeasurementType.DURATION ->
-                                stringResource(Res.string.add_duration)
+                        value = state.name,
+                        onValueChange = listener::onNameChanged,
+                        placeholder = stringResource(Res.string.enter_exercise_name)
+                    )
+                }
 
-                            CreateExerciseState.MeasurementType.REPS ->
-                                stringResource(Res.string.add_reps)
-                        },
-                        keyboardOptions = KeyboardOptions(keyboardType = KeyboardType.Number)
+                item {
+                    RowWithIcon(
+                        modifier = Modifier.padding(bottom = 12.dp),
+                        text = selectedFocusArea,
+                        textColor = selectedFocusAreaTextColor,
+                        isIconClicked = state.isFocusAreaExpanded,
+                        onIconClicked = listener::onFocusAreaIconClicked
+                    )
+                    AnimatedVisibility(visible = state.isFocusAreaExpanded) {
+                        Column {
+                            CustomDropdownMenu(
+                                items = state.focusAreaNames,
+                                onItemSelected = listener::onFocusAreaNameSelected,
+                                isChecked = state::isFocusAreaSelected
+                            )
+                            Spacer(modifier = Modifier.height(8.dp))
+                        }
+                    }
+                }
+
+                item {
+                    Row(
+                        modifier = Modifier
+                            .fillMaxWidth()
+                            .padding(bottom = 12.dp),
+                        horizontalArrangement = Arrangement.spacedBy(8.dp)
+                    ) {
+                        CheckboxItem(
+                            modifier = Modifier.weight(1f),
+                            text = stringResource(Res.string.add_duration),
+                            titleColor = durationTitleColor,
+                            isChecked = state.isDurationChecked,
+                            onCheckedChange = {
+                                if (it) listener.onMeasurementTypeSelected(
+                                    CreateExerciseState.MeasurementType.DURATION
+                                )
+                            },
+                            style = CheckboxStyle.Tick
+                        )
+                        CheckboxItem(
+                            modifier = Modifier.weight(1f),
+                            text = stringResource(Res.string.add_reps),
+                            titleColor = repsTitleColor,
+                            isChecked = state.isRepsChecked,
+                            onCheckedChange = {
+                                if (it) listener.onMeasurementTypeSelected(
+                                    CreateExerciseState.MeasurementType.REPS
+                                )
+                            },
+                            style = CheckboxStyle.Tick
+                        )
+                    }
+                }
+
+                item {
+                    AnimatedVisibility(
+                        visible = state.isDurationChecked || state.isRepsChecked,
+                    ) {
+                        InputField(
+                            modifier = Modifier.padding(bottom = 12.dp),
+                            value = state.measurementInputValue,
+                            onValueChange = listener::onMeasurementValueChanged,
+                            placeholder = when (state.measurementType) {
+                                CreateExerciseState.MeasurementType.DURATION ->
+                                    stringResource(Res.string.add_duration)
+
+                                CreateExerciseState.MeasurementType.REPS ->
+                                    stringResource(Res.string.add_reps)
+                            },
+                            keyboardOptions = KeyboardOptions(keyboardType = KeyboardType.Number)
+                        )
+                    }
+                }
+
+                item {
+                    RowWithIcon(
+                        modifier = Modifier
+                            .fillMaxWidth()
+                            .padding(bottom = 12.dp),
+                        text = selectedEquipmentNames,
+                        textColor = selectedEquipmentTextColor,
+                        isIconClicked = state.isEquipmentExpanded,
+                        onIconClicked = listener::onAvailableEquipmentsIconClicked
+                    )
+                    AnimatedVisibility(visible = state.isEquipmentExpanded) {
+                        Column {
+                            CustomDropdownMenu(
+                                items = state.equipmentNames,
+                                onItemSelected = listener::onEquipmentNameSelected,
+                                isChecked = state::isEquipmentSelected
+                            )
+                            Spacer(modifier = Modifier.height(8.dp))
+                        }
+                    }
+                }
+
+                item {
+                    InputField(
+                        placeholder = stringResource(Res.string.enter_instructions),
+                        minHeight = 124.dp,
+                        value = state.description,
+                        onValueChange = listener::onDescriptionChanged,
+                        isSingleLine = false,
+                        maxCharacters = 3000,
+                        isCharacterCountVisible = true
+                    )
+                }
+
+                item {
+                    PrimaryButton(
+                        modifier = Modifier.padding(vertical = 40.dp),
+                        text = stringResource(Res.string.save_exercise),
+                        onClick = { listener.onSaveClicked()}
                     )
                 }
             }
-
-            item {
-                RowWithIcon(
-                    modifier = Modifier
-                        .fillMaxWidth()
-                        .padding(bottom = 12.dp),
-                    text = selectedEquipmentNames,
-                    textColor = selectedEquipmentTextColor,
-                    isIconClicked = state.isEquipmentExpanded,
-                    onIconClicked = listener::onAvailableEquipmentsIconClicked
-                )
-                AnimatedVisibility(visible = state.isEquipmentExpanded) {
-                    Column {
-                        CustomDropdownMenu(
-                            items = state.equipmentNames,
-                            onItemSelected = listener::onEquipmentNameSelected,
-                            isChecked = state::isEquipmentSelected
-                        )
-                        Spacer(modifier = Modifier.height(8.dp))
-                    }
-
-                }
-            }
-            item {
-                InputField(
-                    placeholder = stringResource(Res.string.enter_instructions),
-                    minHeight = 124.dp,
-                    value = state.description,
-                    onValueChange = listener::onDescriptionChanged,
-                    isSingleLine = false,
-                    maxCharacters = 3000,
-                    isCharacterCountVisible = true
-                )
-            }
-            item {
-                PrimaryButton(
-                    modifier = Modifier.padding(vertical = 40.dp),
-                    text = stringResource(Res.string.save_exercise),
-                    isEnabled = state.isSaveEnabled,
-                    onClick = listener::onSaveClicked
-                )
-            }
         }
+
+        if (state.isImagePickerOpen) {
+            ImagePicker(
+                onImageRetrieved = listener::onImageRetrieved,
+                onImagePickerDismiss = listener::onImagePickerDismiss
+            )
+        }
+
         if (state.showExitBottomSheet) {
             ExiteCreateExerciseBottomSheet(
                 isVisible = state.showExitBottomSheet,
@@ -342,6 +354,7 @@ fun CreateExerciseScreenContent(
         }
     }
 }
+
 
 @Preview()
 @Composable
