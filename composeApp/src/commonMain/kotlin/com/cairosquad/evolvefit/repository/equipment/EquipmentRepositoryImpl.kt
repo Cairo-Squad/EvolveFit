bb1ac18350db792665b5888f8aefd7a1e82c7f64
Hager Abobakr <hagarabobakr1@gmail.com>
2025-08-14 04:24:15 +0300

    implement getAllEquipments in EquipmentRepositoryImpl

--- composeApp/src/commonMain/kotlin/com/cairosquad/evolvefit/repository/equipment/EquipmentRepositoryImpl.kt
+++ composeApp/src/commonMain/kotlin/com/cairosquad/evolvefit/repository/equipment/EquipmentRepositoryImpl.kt
@@ -2,10 +2,16 @@ package com.cairosquad.evolvefit.repository.equipment
 
 import com.cairosquad.evolvefit.domain.entity.Equipment
 import com.cairosquad.evolvefit.domain.repository.EquipmentRepository
+import com.cairosquad.evolvefit.repository.equipment.remot.EquipmentsRemoteDataSource
+import com.cairosquad.evolvefit.repository.equipment.remot.toDomain
+import com.cairosquad.evolvefit.repository.execption.callDataSource
 
-class EquipmentRepositoryImpl: EquipmentRepository {
-    override suspend fun getAllEquipments(): Set<Equipment> {
-        return emptySet()
+class EquipmentRepositoryImpl(
+    private val remote: EquipmentsRemoteDataSource
+): EquipmentRepository {
+    override suspend fun getAllEquipments(): Set<Equipment> = callDataSource{
+        print("EquipmentRepositoryImpl:: getAllEquipments ${remote.getEquipments().map { it.toDomain() }}")
+         remote.getEquipments().map { it.toDomain() }.toSet()
     }
 
     override suspend fun getUserEquipments(): Set<Equipment> {
