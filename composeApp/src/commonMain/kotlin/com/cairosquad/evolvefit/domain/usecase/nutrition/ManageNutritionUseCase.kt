b900cadb6a987f11053921ba7dee26e452f16ee7
Elsayed Magdy <saikoo1239@gmail.com>
2025-08-14 19:04:47 +0300

    refactor : add logic for uses cases and validation

--- composeApp/src/commonMain/kotlin/com/cairosquad/evolvefit/domain/usecase/nutrition/ManageNutritionUseCase.kt
+++ composeApp/src/commonMain/kotlin/com/cairosquad/evolvefit/domain/usecase/nutrition/ManageNutritionUseCase.kt
@@ -1,5 +1,9 @@
 package com.cairosquad.evolvefit.domain.usecase.nutrition
 
+import com.cairosquad.evolvefit.domain.exceptions.ExceededCaloriesException
+import com.cairosquad.evolvefit.domain.exceptions.ExceededWaterLimitException
+import com.cairosquad.evolvefit.domain.exceptions.InvalidNumberFormatException
+import com.cairosquad.evolvefit.domain.exceptions.MealNotFoundException
 import com.cairosquad.evolvefit.domain.repository.NutritionRepository
 import com.cairosquad.evolvefit.entity.nutrition.ConsumedMeal
 import com.cairosquad.evolvefit.entity.nutrition.DailyCalorieSummary
@@ -8,31 +12,72 @@ import com.cairosquad.evolvefit.entity.nutrition.Meal
 import com.cairosquad.evolvefit.entity.nutrition.SuggestedMeal
 
 class ManageNutritionUseCase(private val nutritionRepository: NutritionRepository) {
-    suspend fun getSuggestedMeals(): List<SuggestedMeal>{
+    suspend fun getSuggestedMeals(): List<SuggestedMeal> {
         return nutritionRepository.getSuggestedMeals()
     }
-    suspend fun getFavouriteMeals(): List<SuggestedMeal>{
+
+    suspend fun getFavouriteMeals(): List<SuggestedMeal> {
         return nutritionRepository.getFavouriteMeals()
     }
-    suspend fun getMealHistory(): List<ConsumedMeal>{
+
+    suspend fun getMealHistory(): List<ConsumedMeal> {
         return nutritionRepository.getMealHistory()
     }
-    suspend fun getConsumedMealsByDate(startDate: String, endDate: String): List<ConsumedMeal>{
+
+    suspend fun getConsumedMealsByDate(startDate: String, endDate: String): List<ConsumedMeal> {
         return nutritionRepository.getConsumedMealsByDate(startDate, endDate)
     }
-    suspend fun getMealById(id: String): Meal{
+
+    suspend fun getMealById(id: String): Meal {
         return nutritionRepository.getMealById(id)
+            ?: throw MealNotFoundException(message = "Meal with id=$id not found")
     }
-    suspend fun saveConsumedMeal(consumedMeal: ConsumedMeal): Boolean{
-        return nutritionRepository.saveConsumedMeal(consumedMeal)
+
+    suspend fun saveConsumedMeal(
+        consumedMealCaloriesInput: String,
+        consumedMeal: ConsumedMeal,
+        remainingCalories: Float
+    ): Boolean {
+        validateNumberInput(consumedMealCaloriesInput)
+
+        val enteredCalories = consumedMealCaloriesInput.toFloatOrNull()
+            ?: throw InvalidNumberFormatException()
+
+        if (enteredCalories > remainingCalories) {
+            throw ExceededCaloriesException()
+        }
+        val updatedMeal = consumedMeal.copy(calories = enteredCalories.toInt())
+
+        return nutritionRepository.saveConsumedMeal(updatedMeal)
     }
-    suspend fun getDailyCalorieSummary(): DailyCalorieSummary{
+
+    suspend fun getDailyCalorieSummary(): DailyCalorieSummary {
         return nutritionRepository.getDailyCalorieSummary()
     }
-    suspend fun saveConsumedWater(amountLiters: Float): Boolean{
+
+    suspend fun saveConsumedWater(amountInput: String, remainingWater: Float): Boolean {
+        if (amountInput.contains(',')) {
+            throw InvalidNumberFormatException()
+        }
+
+        val amountLiters = amountInput.toFloatOrNull()
+            ?: throw InvalidNumberFormatException()
+
+        if (amountLiters > remainingWater) {
+            throw ExceededWaterLimitException()
+        }
+
         return nutritionRepository.saveConsumedWater(amountLiters)
     }
-    suspend fun getDailyWaterSummary(): DailyWaterSummary{
+
+    suspend fun getDailyWaterSummary(): DailyWaterSummary {
         return nutritionRepository.getDailyWaterSummary()
+    }
+}
+
+private fun validateNumberInput(value: String) {
+    val regex = Regex("^[0-9]*\\.?[0-9]*$")
+    if (!regex.matches(value)) {
+        throw InvalidNumberFormatException()
     }
 }
