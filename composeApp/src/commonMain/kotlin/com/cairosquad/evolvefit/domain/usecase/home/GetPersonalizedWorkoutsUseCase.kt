b7467481153fd27165f4d48cda213f9926bbec92
Shahd-Hatem <Shahdhatim863@gmail.com>
2025-08-15 03:52:19 +0300

    update GetPersonalizedWorkoutsUseCase

--- composeApp/src/commonMain/kotlin/com/cairosquad/evolvefit/domain/usecase/home/GetPersonalizedWorkoutsUseCase.kt
+++ composeApp/src/commonMain/kotlin/com/cairosquad/evolvefit/domain/usecase/home/GetPersonalizedWorkoutsUseCase.kt
@@ -1,13 +1,14 @@
 package com.cairosquad.evolvefit.domain.usecase.home
 
 import com.cairosquad.evolvefit.domain.entity.Workout
+import com.cairosquad.evolvefit.domain.entity.WorkoutSuggested
 import com.cairosquad.evolvefit.domain.repository.WorkoutRepository
 
 class GetPersonalizedWorkoutsUseCase(
     private val workoutRepository: WorkoutRepository
 ) {
 
-    suspend fun getWorkouts(): List<Workout> {
+    suspend fun getWorkouts(): List<WorkoutSuggested> {
         return workoutRepository.getSuggestedWorkouts()
     }
 }
