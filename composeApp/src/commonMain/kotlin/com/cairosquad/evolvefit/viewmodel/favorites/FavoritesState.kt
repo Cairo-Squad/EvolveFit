65a3dd5a265b71785477a1f1274683541b0d407a
Hager Abobakr <hagarabobakr1@gmail.com>
2025-08-15 09:23:15 +0300

    add FavoritesState and related data classes

---
+++ composeApp/src/commonMain/kotlin/com/cairosquad/evolvefit/viewmodel/favorites/FavoritesState.kt
@@ -0,0 +1,38 @@
+package com.cairosquad.evolvefit.viewmodel.favorites
+
+data class FavoritesState(
+   val isLoading: Boolean = false,
+    val workoutsList: List<WorkoutsUiModel> = emptyList(),
+    val mealsList: List<MealsUiModel> = emptyList(),
+    val isWorkoutTabSelected: Boolean = true,
+    val isMealTabSelected: Boolean = false
+)
+
+data class WorkoutsUiModel(
+    val name: String,
+    val estimatedTimeInSeconds: Int,
+    val focusArea: String,
+    val imageUrl: String
+)
+
+data class MealsUiModel(
+    val name: String,
+    val type: MealType,
+    val imageUrl: String,
+    val nutrition: Nutrition,
+){
+    data class Nutrition(
+        val caloriesInKcal: Int,
+        val fatInGrams: Int,
+        val proteinInGrams: Int,
+        val carbsInGrams: Int
+    )
+}
+
+
+enum class MealType {
+    BREAKFAST,
+    LUNCH,
+    DINNER,
+    SNACKS
+}
