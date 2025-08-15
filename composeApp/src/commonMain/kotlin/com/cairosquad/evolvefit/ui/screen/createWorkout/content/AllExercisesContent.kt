530916731844c156c992f75d07451e1d2b39f699
Shahd-Hatem <Shahdhatim863@gmail.com>
2025-08-15 03:32:18 +0300

    create CreateWorkoutScreen.kt

---
+++ composeApp/src/commonMain/kotlin/com/cairosquad/evolvefit/ui/screen/createWorkout/content/AllExercisesContent.kt
@@ -0,0 +1,284 @@
+package com.cairosquad.evolvefit.ui.screen.createWorkout.content
+
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.statusBarsPadding
+import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.lazy.items
+import androidx.compose.foundation.text.BasicText
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.unit.dp
+import com.cairosquad.evolvefit.design_system.component.ExerciseCard
+import com.cairosquad.evolvefit.design_system.component.PrimaryButton
+import com.cairosquad.evolvefit.design_system.component.StateMessage
+import com.cairosquad.evolvefit.design_system.component.appbar.ActionIconButton
+import com.cairosquad.evolvefit.design_system.component.appbar.CustomAppBar
+import com.cairosquad.evolvefit.design_system.composables.InputField
+import com.cairosquad.evolvefit.design_system.theme.Theme
+import com.cairosquad.evolvefit.domain.entity.Exercise
+import com.cairosquad.evolvefit.viewmodel.createWorkOut.CreateWorkOutInteractionListener
+import com.cairosquad.evolvefit.viewmodel.createWorkOut.CreateWorkOutScreenState
+import com.cairosquad.evolvefit.viewmodel.createWorkOut.CreateWorkOutScreenState.ScreenStatus
+import com.cairosquad.evolvefit.viewmodel.onboarding.models.UiImage
+import evolvefit.composeapp.generated.resources.Res
+import evolvefit.composeapp.generated.resources.add_exercise_button_
+import evolvefit.composeapp.generated.resources.add_exercise_button_with_count_
+import evolvefit.composeapp.generated.resources.add_exercise_title_
+import evolvefit.composeapp.generated.resources.add_exercises_button_with_count_
+import evolvefit.composeapp.generated.resources.add_icon_desc_
+import evolvefit.composeapp.generated.resources.all_exercises_title_
+import evolvefit.composeapp.generated.resources.back_icon_desc_
+import evolvefit.composeapp.generated.resources.ic_addcircle
+import evolvefit.composeapp.generated.resources.ic_back
+import evolvefit.composeapp.generated.resources.ic_error
+import evolvefit.composeapp.generated.resources.ic_search
+import evolvefit.composeapp.generated.resources.search_exercise_placeholder_
+import org.jetbrains.compose.resources.painterResource
+import org.jetbrains.compose.resources.stringResource
+import org.jetbrains.compose.ui.tooling.preview.Preview
+
+
+@Composable
+fun AllExercisesContent(
+    state: CreateWorkOutScreenState,
+    listener: CreateWorkOutInteractionListener,
+) {
+    Column(
+        modifier = Modifier
+            .fillMaxSize()
+            .background(color = Theme.color.surfaces.surface)
+            .statusBarsPadding(),
+        verticalArrangement = Arrangement.SpaceBetween,
+        horizontalAlignment = Alignment.CenterHorizontally,
+    ) {
+        Column(
+            verticalArrangement = Arrangement.spacedBy(12.dp)
+        ) {
+            CustomAppBar(
+                title = stringResource(Res.string.add_exercise_title_),
+                header = {
+                    ActionIconButton(
+                        icon = painterResource(Res.drawable.ic_back),
+                        contentDescription = stringResource(Res.string.back_icon_desc_),
+                        tint = Theme.color.surfaces.onSurface,
+                        onClick = listener::onBackClicked
+                    )
+                },
+                tail = {
+                    ActionIconButton(
+                        icon = painterResource(Res.drawable.ic_addcircle),
+                        contentDescription = stringResource(Res.string.add_icon_desc_),
+                        tint = Theme.color.surfaces.onSurface,
+                        onClick = listener::onAddClicked
+                    )
+                },
+            )
+
+            InputField(
+                value = state.searchQuery,
+                onValueChange = listener::onSearchQueryChanged,
+                placeholder = stringResource(Res.string.search_exercise_placeholder_),
+                leadingIcon = Res.drawable.ic_search,
+                modifier = Modifier.padding(horizontal = 16.dp)
+            )
+
+            BasicText(
+                text = stringResource(Res.string.all_exercises_title_),
+                style = Theme.textStyle.label.smallRegular14.copy(
+                    color = Theme.color.surfaces.onSurfaceVariant
+                ),
+                modifier = Modifier.padding(horizontal = 16.dp)
+            )
+
+            Box(
+                modifier = Modifier
+                    .fillMaxSize()
+                    .weight(1f),
+                contentAlignment = Alignment.Center
+            ) {
+                when (state.status) {
+                    ScreenStatus.LOADING -> {
+                        // Loading indicator
+                    }
+                    ScreenStatus.EMPTY -> {
+                        StateMessage(
+                            image = painterResource(Res.drawable.ic_error),
+                            title = "No exercises found",
+                            description = "Try adjusting your search terms or make sure the exercise name is spelled correctly.",
+                            modifier = Modifier.fillMaxWidth()
+                        )
+                    }
+                    ScreenStatus.SUCCESS -> {
+                        LazyColumn(
+                            modifier = Modifier
+                                .padding(horizontal = 16.dp)
+                        ) {
+                            items(state.filteredExercises) { exercise ->
+                                val isChecked = state.selectedExercises.any { it.id == exercise.id }
+
+                                ExerciseCard(
+                                    title = exercise.name,
+                                    model = exercise.imageUrls.toString(),
+                                    timeInSeconds = when (exercise.specification) {
+                                        is Exercise.Specification.Reps -> null
+                                        is Exercise.Specification.Time -> exercise.specification.timeInSeconds
+                                    },
+                                    reps = when (exercise.specification) {
+                                        is Exercise.Specification.Reps -> exercise.specification.reps
+                                        is Exercise.Specification.Time -> null
+                                    },
+                                    sets = null,
+                                    isChecked = isChecked,
+                                    onCheckedChange = {
+                                        listener.onExerciseCheckedChanged(exercise)
+                                    },
+                                )
+                            }
+                        }
+                    }
+                    ScreenStatus.ERROR -> {
+                        // Error UI
+                    }
+                }
+            }
+        }
+
+        PrimaryButton(
+            text = when {
+                state.exerciseCount > 1 -> stringResource(Res.string.add_exercises_button_with_count_, state.exerciseCount)
+                state.exerciseCount == 1 -> stringResource(Res.string.add_exercise_button_with_count_, state.exerciseCount)
+                else -> stringResource(Res.string.add_exercise_button_)
+            },
+            onClick = { listener.onAddWorkoutClicked() },
+            isEnabled = state.exerciseCount > 0,
+            modifier = Modifier
+                .fillMaxWidth()
+                .padding(horizontal = 16.dp, vertical = 16.dp)
+                .padding(bottom = 24.dp)
+        )
+    }
+}
+
+@Preview()
+@Composable
+fun PreviewAllExercisesContent_Empty() {
+    AllExercisesContent(
+        state = CreateWorkOutScreenState(
+            searchQuery = "",
+            filteredExercises = emptyList(),
+            selectedExercises = emptyList(),
+            exerciseCount = 0,
+            status = ScreenStatus.EMPTY
+        ),
+        listener = object : CreateWorkOutInteractionListener {
+            override fun onNameChanged(newName: String) {
+                TODO("Not yet implemented")
+            }
+
+            override fun onGoalSelected(goal: String) {
+                TODO("Not yet implemented")
+            }
+
+            override fun onDescriptionChanged(desc: String) {
+                TODO("Not yet implemented")
+            }
+
+            override fun onImageClicked() {
+                TODO("Not yet implemented")
+            }
+
+            override fun onNextClicked() {
+                TODO("Not yet implemented")
+            }
+
+            override fun onBackClicked() {}
+            override fun onAddClicked() {}
+            override fun onSearchQueryChanged(query: String) {}
+            override fun onExerciseCheckedChanged(exercise: Exercise) {}
+            override fun onAddWorkoutClicked() {}
+            override fun onImageSelected(image: UiImage) {
+                TODO("Not yet implemented")
+            }
+
+            override fun onImagePickerDismiss() {
+                TODO("Not yet implemented")
+            }
+        }
+    )
+}
+
+@Preview()
+@Composable
+fun PreviewAllExercisesContent_Success() {
+    AllExercisesContent(
+        state = CreateWorkOutScreenState(
+            searchQuery = "",
+            filteredExercises = listOf(
+                Exercise(
+                    id = "1",
+                    name = "Push Ups",
+                    imageUrls = listOf("https://via.placeholder.com/150"),
+                    specification = Exercise.Specification.Reps(reps = 10),
+                    instructions = TODO(),
+                    equipment = TODO(),
+                    focusAreas = TODO(),
+                    estimatedTimeInSeconds = TODO()
+                ),
+                Exercise(
+                    id = "2",
+                    name = "Plank",
+                    imageUrls = listOf("https://via.placeholder.com/150"),
+                    specification = Exercise.Specification.Time(timeInSeconds = 60),
+                    instructions = TODO(),
+                    equipment = TODO(),
+                    focusAreas = TODO(),
+                    estimatedTimeInSeconds = TODO()
+                )
+            ),
+            selectedExercises = emptyList(),
+            exerciseCount = 0,
+            status = ScreenStatus.SUCCESS
+        ),
+        listener = object : CreateWorkOutInteractionListener {
+            override fun onNameChanged(newName: String) {
+                TODO("Not yet implemented")
+            }
+
+            override fun onGoalSelected(goal: String) {
+                TODO("Not yet implemented")
+            }
+
+            override fun onDescriptionChanged(desc: String) {
+                TODO("Not yet implemented")
+            }
+
+            override fun onImageClicked() {
+                TODO("Not yet implemented")
+            }
+
+            override fun onNextClicked() {
+                TODO("Not yet implemented")
+            }
+
+            override fun onBackClicked() {}
+            override fun onAddClicked() {}
+            override fun onSearchQueryChanged(query: String) {}
+            override fun onExerciseCheckedChanged(exercise: Exercise) {}
+            override fun onAddWorkoutClicked() {}
+            override fun onImageSelected(image: UiImage) {
+                TODO("Not yet implemented")
+            }
+
+            override fun onImagePickerDismiss() {
+                TODO("Not yet implemented")
+            }
+        }
+    )
+}
---
+++ composeApp/src/commonMain/kotlin/com/cairosquad/evolvefit/ui/screen/createWorkout/content/CreateWorkoutContent.kt
@@ -0,0 +1,177 @@
+package com.cairosquad.evolvefit.ui.screen.createWorkout.content
+
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.WindowInsets
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.layout.statusBars
+import androidx.compose.foundation.layout.windowInsetsPadding
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.foundation.text.BasicText
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
+import androidx.compose.ui.unit.dp
+import com.cairosquad.evolvefit.design_system.component.CustomDropDownMenu
+import com.cairosquad.evolvefit.design_system.component.PrimaryButton
+import com.cairosquad.evolvefit.design_system.component.appbar.ActionIconButton
+import com.cairosquad.evolvefit.design_system.component.appbar.CustomAppBar
+import com.cairosquad.evolvefit.design_system.composables.InputField
+import com.cairosquad.evolvefit.design_system.theme.Theme
+import com.cairosquad.evolvefit.ui.component.UserProfileImage
+import com.cairosquad.evolvefit.viewmodel.createWorkOut.CreateWorkOutInteractionListener
+import com.cairosquad.evolvefit.viewmodel.createWorkOut.CreateWorkOutScreenState
+import com.cairosquad.evolvefit.viewmodel.createWorkOut.CreateWorkOutScreenState.WorkoutLevel
+import com.cairosquad.evolvefit.viewmodel.onboarding.models.UiImage.ImageResource
+import evolvefit.composeapp.generated.resources.Res
+import evolvefit.composeapp.generated.resources.characters_left_
+import evolvefit.composeapp.generated.resources.choose_your_goal_
+import evolvefit.composeapp.generated.resources.create_workout_subtitle_
+import evolvefit.composeapp.generated.resources.create_workout_title_
+import evolvefit.composeapp.generated.resources.cross_icon_desc_
+import evolvefit.composeapp.generated.resources.enter_description_
+import evolvefit.composeapp.generated.resources.enter_workout_name_
+import evolvefit.composeapp.generated.resources.ic_arrow_down
+import evolvefit.composeapp.generated.resources.ic_cross
+import evolvefit.composeapp.generated.resources.ic_image
+import evolvefit.composeapp.generated.resources.next_button_
+import org.jetbrains.compose.resources.painterResource
+import org.jetbrains.compose.resources.stringResource
+
+@Composable
+fun CreateWorkoutContent(
+    state: CreateWorkOutScreenState,
+    listener: CreateWorkOutInteractionListener,
+) {
+    val workoutGoals = WorkoutLevel.entries.toTypedArray()
+    Column(
+        modifier = Modifier
+            .fillMaxSize()
+            .background(Theme.color.surfaces.surface)
+            .windowInsetsPadding(WindowInsets.statusBars),
+        verticalArrangement = Arrangement.SpaceBetween,
+        horizontalAlignment = Alignment.CenterHorizontally
+    ) {
+
+        Column {
+            CustomAppBar(
+                title = "",
+                header = {
+                    ActionIconButton(
+                        icon = painterResource(Res.drawable.ic_cross),
+                        contentDescription = stringResource(Res.string.cross_icon_desc_),
+                        tint = Theme.color.surfaces.onSurface,
+                        onClick = listener::onBackClicked,
+                    )
+                },
+                modifier = Modifier
+                    .padding(bottom = 16.dp)
+                    .padding(horizontal = 16.dp)
+            )
+
+            Column(
+                modifier = Modifier
+                    .padding(bottom = 24.dp)
+                    .padding(horizontal = 16.dp)
+            ) {
+                BasicText(
+                    text = stringResource(Res.string.create_workout_title_),
+                    style = Theme.textStyle.headline.mediumMedium18.copy(
+                        color = Theme.color.surfaces.onSurface
+                    )
+                )
+
+                BasicText(
+                    text = stringResource(Res.string.create_workout_subtitle_),
+                    style = Theme.textStyle.label.smallRegular14.copy(
+                        color = Theme.color.surfaces.onSurfaceVariant
+                    ),
+                )
+
+            }
+
+            UserProfileImage(
+                image = state.image ?: ImageResource(Res.drawable.ic_image),
+                isImagePickerOpen = state.isImagePickerOpen,
+                onImagePickerDismiss = listener::onImagePickerDismiss,
+                onImagePickerClick = listener::onImageClicked,
+                onImageRetrieved = listener::onImageSelected,
+                modifier = Modifier
+                    .align(Alignment.CenterHorizontally)
+                    .size(100.dp)
+                    .clip(shape = RoundedCornerShape(8.dp))
+            )
+
+            InputField(
+                value = state.name,
+                onValueChange = listener::onNameChanged,
+                placeholder = stringResource(Res.string.enter_workout_name_),
+                modifier = Modifier.padding(horizontal = 16.dp, vertical = 8.dp)
+            )
+
+            CustomDropDownMenu(
+                selectedText = state.goal.name,
+                options = workoutGoals.map { it.name },
+                placeholder = stringResource(Res.string.choose_your_goal_),
+                iconPainter = painterResource(Res.drawable.ic_arrow_down),
+                onOptionSelected = { selectedGoal ->
+                    listener.onGoalSelected(selectedGoal)
+                },
+                modifier = Modifier.padding(horizontal = 16.dp, vertical = 8.dp),
+            )
+
+            Box(
+                modifier = Modifier
+                    .padding(horizontal = 16.dp, vertical = 8.dp)
+                    .background(
+                        color = Theme.color.surfaces.surfaceContainer,
+                        shape = RoundedCornerShape(8.dp)
+                    )
+                    .clip(RoundedCornerShape(8.dp))
+            ) {
+                InputField(
+                    value = state.description,
+                    onValueChange = {
+                        if (it.length <= 3000) listener.onDescriptionChanged(it)
+                    },
+                    maxCharacters = 3000,
+                    isSingleLine = false,
+                    placeholder = stringResource(Res.string.enter_description_),
+                    modifier = Modifier
+                        .height(124.dp)
+                        .fillMaxWidth()
+                )
+                BasicText(
+                    text = stringResource(
+                        Res.string.characters_left_,
+                        3000 - state.description.length
+                    ),
+                    style = Theme.textStyle.label.smallRegular12.copy(
+                        color = Theme.color.surfaces.onSurfaceVariant
+                    ),
+                    modifier = Modifier
+                        .align(Alignment.BottomEnd)
+                        .padding(bottom = 12.dp)
+                        .padding(horizontal = 12.dp)
+                )
+            }
+        }
+
+        PrimaryButton(
+            text = stringResource(Res.string.next_button_),
+            onClick = listener::onNextClicked,
+            isEnabled = state.isNextEnabled,
+            modifier = Modifier
+                .fillMaxWidth()
+                .padding(horizontal = 16.dp, vertical = 16.dp)
+                .padding(bottom = 24.dp)
+        )
+    }
+}
