a9a1e7a93692391592f96641da5a13c20f5d89c3
Shahd-Hatem <Shahdhatim863@gmail.com>
2025-08-15 04:35:22 +0300

    solve of falling build

---
+++ composeApp/src/commonMain/kotlin/com/cairosquad/evolvefit/viewmodel/exercise/CreateExerciseViewModel.kt
@@ -0,0 +1,115 @@
+package com.cairosquad.evolvefit.viewmodel.exercise
+
+import com.cairosquad.evolvefit.domain.usecase.equipment.ManageEquipmentUseCase
+import com.cairosquad.evolvefit.domain.usecase.exercise.ManageExerciseUseCase
+import com.cairosquad.evolvefit.viewmodel.base.BaseViewModel
+import com.cairosquad.evolvefit.viewmodel.exercise.CreateExerciseState.FocusArea
+import com.cairosquad.evolvefit.viewmodel.exercise.CreateExerciseState.MeasurementType
+import com.cairosquad.evolvefit.viewmodel.onboarding.models.UiImage
+
+class CreateExerciseViewModel(
+    private val manageExerciseUseCase: ManageExerciseUseCase,
+    private val manageEquipmentUseCase: ManageEquipmentUseCase,
+) : BaseViewModel<CreateExerciseState, CreateExerciseEffect>(CreateExerciseState()),
+    CreateExerciseInteractionListener {
+
+    init {
+        getEquipments()
+    }
+
+    private fun getEquipments() {
+        tryToCall(
+            block = { manageEquipmentUseCase.getAllEquipments() },
+            onSuccess = { equipments ->
+                updateState { it.copy(availableEquipments = equipments.map { it.toUiState() }.toSet() ) }
+            },
+            onError = {
+                updateState { it.copy(errorMessage = "Failed to load equipments") }
+            }
+        )
+    }
+
+    override fun onEquipmentToggled(equipmentId: Int) {
+        updateState {
+            val updatedEquipment = it.availableEquipments
+                .firstOrNull { it.id == equipmentId }
+                ?: CreateExerciseState.EquipmentUiState()
+            it.copy(selectedEquipment = updatedEquipment)
+        }
+    }
+
+    override fun onImagePickerClicked() {
+        updateState { it.copy(isImagePickerOpen = true) }
+    }
+
+    override fun onImagePickerDismiss() {
+        updateState { it.copy(isImagePickerOpen = false) }
+    }
+
+    override fun onFocusAreaToggled(focusArea: FocusArea) {
+        updateState {
+            val updatedSelection = it.selectedFocusAreas.toMutableSet()
+            if (focusArea in updatedSelection) {
+                updatedSelection.remove(focusArea)
+            } else {
+                updatedSelection.add(focusArea)
+            }
+            it.copy(selectedFocusAreas = updatedSelection)
+        }
+    }
+
+    override fun onDescriptionChanged(description: String) {
+        updateState { it.copy(description = description) }
+    }
+
+    override fun onNameChanged(name: String) {
+        updateState { it.copy(name = name) }
+    }
+
+    override fun onImagePicked(image: UiImage) {
+        updateState { it.copy(image = image, isImagePickerOpen = false) }
+    }
+
+    override fun onMeasurementTypeSelected(type: MeasurementType) {
+        updateState { it.copy(measurementType = type) }
+    }
+
+    override fun onMeasurementValueChanged(value: String) {
+        updateState { it.copy(measurementInputValue = value) }
+    }
+
+    override fun onSaveClicked() {
+        tryToCall(
+            block = ::saveExercise,
+            onSuccess = {
+                sendEffect(CreateExerciseEffect.NavigateToAllExercises)
+            },
+            onError = { e ->
+                sendEffect(CreateExerciseEffect.ShowError(e.message ?: "Unknown error"))
+            },
+            onStart = {
+                updateState { it.copy(isExerciseSaved = true) }
+            },
+            onEnd = {
+                updateState { it.copy(isExerciseSaved = false) }
+            }
+        )
+    }
+
+    private suspend fun saveExercise() =
+        manageExerciseUseCase.createExercise(screenState.value.toDomainExercise())
+
+    override fun onExitClicked() {
+        updateState { it.copy(showExitBottomSheet = true) }
+    }
+
+    override fun onExitOptionSelected(saveBeforeExit: Boolean) {
+        updateState { it.copy(showExitBottomSheet = false) }
+
+        if (saveBeforeExit) {
+            onSaveClicked()
+        } else {
+            sendEffect(CreateExerciseEffect.CloseScreen)
+        }
+    }
+}
---
+++ composeApp/src/commonMain/kotlin/com/cairosquad/evolvefit/viewmodel/exercise/mapper.kt
@@ -0,0 +1,91 @@
+package com.cairosquad.evolvefit.viewmodel.exercise
+
+import com.cairosquad.evolvefit.domain.entity.Equipment
+import com.cairosquad.evolvefit.domain.entity.Exercise
+import com.cairosquad.evolvefit.domain.model.FocusArea
+import com.cairosquad.evolvefit.viewmodel.onboarding.models.UiImage
+import io.github.vinceglb.filekit.path
+
+fun CreateExerciseState.toDomainExercise(): Exercise {
+    val timeOrReps = measurementInputValue.toIntOrNull() ?: 0
+
+    val imageUrl = when (val img = this.image) {
+        is UiImage.ImageUrl -> img.url
+        is UiImage.ImageResource -> ""
+        is UiImage.ImageFile -> img.platformFile.path
+        null -> ""
+    }
+    return Exercise(
+        name = this.name,
+        imageUrls = listOfNotNull(imageUrl.takeIf { it.isNotBlank() }),
+        equipment = this.selectedEquipment.toDomain(),
+        specification = when (measurementType) {
+            CreateExerciseState.MeasurementType.DURATION -> Exercise.Specification.Time(
+                measurementInputValue.toIntOrNull() ?: 0
+            )
+
+            CreateExerciseState.MeasurementType.REPS -> Exercise.Specification.Reps(
+                measurementInputValue.toInt()
+            )
+        },
+        focusAreas = this.selectedFocusAreas.map { it.toDomain() }.toSet(),
+        instructions = this.description.split("\n"),
+        id = "",
+        estimatedTimeInSeconds = when (measurementType) {
+            CreateExerciseState.MeasurementType.DURATION -> timeOrReps
+            CreateExerciseState.MeasurementType.REPS -> timeOrReps * 3
+        }
+    )
+}
+
+
+private fun CreateExerciseState.FocusArea.toDomain(): FocusArea {
+    return when (this) {
+        CreateExerciseState.FocusArea.Quadriceps -> FocusArea.SHOULDERS
+        CreateExerciseState.FocusArea.Abs -> FocusArea.CORE
+        CreateExerciseState.FocusArea.Calves -> FocusArea.ARMS
+        CreateExerciseState.FocusArea.LowerBack -> FocusArea.BACK
+        CreateExerciseState.FocusArea.Core -> FocusArea.LEGS
+        CreateExerciseState.FocusArea.Shoulders -> FocusArea.CHEST
+    }
+}
+
+fun CreateExerciseState.EquipmentUiState.toDomain(): Equipment {
+    return Equipment(
+        id = this.id,
+        name = this.name
+    )
+}
+
+fun Equipment.toUiState(): CreateExerciseState.EquipmentUiState {
+    return CreateExerciseState.EquipmentUiState(
+        id = this.id,
+        name = this.name
+    )
+}
+
+fun List<CreateExerciseState.EquipmentUiState>.toDomainList(): List<Equipment> {
+    return this.map { it.toDomain() }
+}
+
+//fun Exercise.toUiExercise(): CreateExerciseState.ExerciseUiModel {
+//    val measurementType = when (specification) {
+//        is Exercise.Specification.Time -> CreateExerciseState.MeasurementType.DURATION
+//        is Exercise.Specification.Reps -> CreateExerciseState.MeasurementType.REPS
+//    }
+//
+//    val measurementValue = when (specification) {
+//        is Exercise.Specification.Time -> specification.timeInSeconds
+//        is Exercise.Specification.Reps -> specification.reps
+//    }
+//
+//    return CreateExerciseState.ExerciseUiModel(
+//        id = id.toIntOrNull() ?: 0,
+//        name = name,
+//        image = imageUrls.firstOrNull()?.let { UiImage.ImageUrl(it) },
+//        equipments = listOf(equipment.name),
+//        focusAreas = focusAreas.toList(),
+//        measurementType = measurementType,
+//        measurementValue = measurementValue,
+//        description = instructions.joinToString("\n")
+//    )
