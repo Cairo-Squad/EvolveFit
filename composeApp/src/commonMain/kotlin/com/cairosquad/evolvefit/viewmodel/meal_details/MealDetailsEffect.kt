782f97ab5773eb5b12b0698fda17d09d9fefcbd5
abdelrahman <abdelrahmankarim7@gmail.com>
2025-08-16 14:29:25 +0300

    feat : add meal details interaction listener and effects

---
+++ composeApp/src/commonMain/kotlin/com/cairosquad/evolvefit/viewmodel/meal_details/MealDetailsEffect.kt
@@ -0,0 +1,5 @@
+package com.cairosquad.evolvefit.viewmodel.meal_details
+
+sealed class MealDetailsEffect {
+    object NavigateBack : MealDetailsEffect()
+}
---
+++ composeApp/src/commonMain/kotlin/com/cairosquad/evolvefit/viewmodel/meal_details/MealDetailsInteractionListener.kt
@@ -0,0 +1,6 @@
+package com.cairosquad.evolvefit.viewmodel.meal_details
+
+interface MealDetailsInteractionListener {
+    fun onBackClicked()
+    fun onSaveMealClicked(mealId: String)
+}
