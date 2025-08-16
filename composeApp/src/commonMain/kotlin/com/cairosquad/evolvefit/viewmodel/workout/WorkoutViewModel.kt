3bc0f54581907ec3a1a18c3d5a32df08e5cb8a1c
Shahd-Hatem <Shahdhatim863@gmail.com>
2025-08-16 11:03:10 +0300

    update WorkoutsScreen

--- composeApp/src/commonMain/kotlin/com/cairosquad/evolvefit/viewmodel/workout/WorkoutViewModel.kt
+++ composeApp/src/commonMain/kotlin/com/cairosquad/evolvefit/viewmodel/workout/WorkoutViewModel.kt
@@ -1,9 +1,9 @@
 package com.cairosquad.evolvefit.viewmodel.workout
 
-import com.cairosquad.evolvefit.domain.entity.Workout
 import com.cairosquad.evolvefit.domain.entity.WorkoutSuggested
 import com.cairosquad.evolvefit.domain.usecase.workout.ManageWorkoutUseCase
 import com.cairosquad.evolvefit.viewmodel.base.BaseViewModel
+import com.cairosquad.evolvefit.viewmodel.workout.WorkoutScreenState.FocusAreaUiState
 
 class WorkoutViewModel(
     private val workoutUseCase: ManageWorkoutUseCase,
@@ -12,7 +12,6 @@ class WorkoutViewModel(
     WorkoutScreenState()
 ), WorkoutInteractionListener {
     init {
-        println("WorkoutViewModel")
         loadAllWorkouts()
     }
 
@@ -24,25 +23,18 @@ class WorkoutViewModel(
         )
     }
 
-    private fun loadWorkoutsByFocusArea(focusAreaUiState: WorkoutScreenState.FocusAreaUiState) {
+    private fun loadWorkoutsByFocusArea(focusAreaUiState: FocusAreaUiState) {
         tryToCall(
-            block = {
-                TODO()
-//                if (focusAreaUiState == WorkoutScreenState.FocusAreaUiState.FULL_BODY) {
-//                    workoutUseCase.getSuggestedWorkouts()
-//                } else {
-//                    workoutUseCase.getWorkoutsByFocusArea(focusAreaUiState.toDomain())
-//                }
-            },
+            block = { workoutUseCase.getWorkoutsByFocusArea(focusAreaUiState.toDomain()) },
             onSuccess = ::onLoadWorkoutByFocusAreaSuccess,
             onError = ::onLoadWorkoutByFocusAreaError
         )
     }
 
-    override fun onSelectFocusArea(focusArea: WorkoutScreenState.FocusAreaUiState) {
+    override fun onSelectFocusArea(focusArea: FocusAreaUiState) {
         updateState { it.copy(selectedFocusArea = focusArea) }
 
-        if (focusArea == WorkoutScreenState.FocusAreaUiState.CORE) {
+        if (focusArea == FocusAreaUiState.CORE) {
             loadAllWorkouts()
         } else {
             loadWorkoutsByFocusArea(focusArea)
@@ -62,21 +54,15 @@ class WorkoutViewModel(
     }
 
     private fun onGetSuggestedWorkoutsSuccess(workouts: List<WorkoutSuggested>) {
-        println("DEBUG: Received ${workouts.size} workouts from API")
-        workouts.forEach {
-            println("DEBUG: Workout name = ${it.name}")
-        }
         updateState { st -> st.copy(allWorkouts = workouts.map { it.toUiState() }) }
     }
 
     private fun onGetSuggestedWorkoutError(t: Throwable) {
-        println("Error fetching suggested workouts")
-        println("Error:$t")
         // TODO: show snackbar/effect
     }
 
-    private fun onLoadWorkoutByFocusAreaSuccess(workouts: List<Workout>) {
-        //updateState { st -> st.copy(allWorkouts = workouts.map { it.toUiState() }) }
+    private fun onLoadWorkoutByFocusAreaSuccess(workouts: List<WorkoutSuggested>) {
+        updateState { st -> st.copy(allWorkouts = workouts.map { it.toUiState() }) }
     }
 
     private fun onLoadWorkoutByFocusAreaError(t: Throwable) {
