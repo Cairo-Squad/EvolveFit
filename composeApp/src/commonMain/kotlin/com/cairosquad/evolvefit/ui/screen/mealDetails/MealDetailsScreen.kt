e77a2c9bf30040c39e71720a21fbb3091d2e34e7
abdelrahman <abdelrahmankarim7@gmail.com>
2025-08-16 14:30:09 +0300

    refactor : remove unused imports

--- composeApp/src/commonMain/kotlin/com/cairosquad/evolvefit/ui/screen/mealDetails/MealDetailsScreen.kt
+++ composeApp/src/commonMain/kotlin/com/cairosquad/evolvefit/ui/screen/mealDetails/MealDetailsScreen.kt
@@ -1,23 +1,15 @@
 package com.cairosquad.evolvefit.ui.screen.mealDetails
 
 import androidx.compose.foundation.background
-import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
-import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.Spacer
-import androidx.compose.foundation.layout.WindowInsets
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.offset
 import androidx.compose.foundation.layout.padding
-import androidx.compose.foundation.layout.statusBars
-import androidx.compose.foundation.layout.windowInsetsPadding
-import androidx.compose.foundation.lazy.LazyColumn
-import androidx.compose.foundation.lazy.items
 import androidx.compose.foundation.shape.RoundedCornerShape
-import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.LaunchedEffect
 import androidx.compose.runtime.collectAsState
@@ -25,10 +17,7 @@ import androidx.compose.runtime.getValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.layout.ContentScale
-import androidx.compose.ui.text.style.TextOverflow
 import androidx.compose.ui.unit.dp
-import com.cairosquad.evolvefit.design_system.component.appbar.ActionIconButton
-import com.cairosquad.evolvefit.design_system.component.appbar.CustomAppBar
 import com.cairosquad.evolvefit.design_system.theme.Theme
 import com.cairosquad.evolvefit.design_system.util.NetworkImage
 import com.cairosquad.evolvefit.ui.screen.mealDetails.components.IngredientsList
@@ -41,13 +30,7 @@ import com.cairosquad.evolvefit.viewmodel.meal_details.MealDetailsInteractionLis
 import com.cairosquad.evolvefit.viewmodel.meal_details.MealDetailsScreenState
 import com.cairosquad.evolvefit.viewmodel.meal_details.MealDetailsViewModel
 import evolvefit.composeapp.generated.resources.Res
-import evolvefit.composeapp.generated.resources.back
-import evolvefit.composeapp.generated.resources.bookmark
-import evolvefit.composeapp.generated.resources.ic_back
-import evolvefit.composeapp.generated.resources.ic_bookmark
-import evolvefit.composeapp.generated.resources.ingredients
 import evolvefit.composeapp.generated.resources.meal_image
-import org.jetbrains.compose.resources.painterResource
 import org.jetbrains.compose.resources.stringResource
 import org.koin.compose.viewmodel.koinViewModel
 
--- composeApp/src/commonMain/kotlin/com/cairosquad/evolvefit/viewmodel/meal_details/MealDetailsViewModel.kt
+++ composeApp/src/commonMain/kotlin/com/cairosquad/evolvefit/viewmodel/meal_details/MealDetailsViewModel.kt
@@ -1,16 +1,8 @@
 package com.cairosquad.evolvefit.viewmodel.meal_details
 
-import androidx.lifecycle.viewModelScope
-import com.cairosquad.evolvefit.domain.usecase.meal.ManageMealUseCase
 import com.cairosquad.evolvefit.domain.usecase.nutrition.ManageNutritionUseCase
 import com.cairosquad.evolvefit.entity.nutrition.Meal
-import com.cairosquad.evolvefit.entity.nutrition.SuggestedMeal
 import com.cairosquad.evolvefit.viewmodel.base.BaseViewModel
-import com.cairosquad.evolvefit.viewmodel.meal_details.toMealDetailsUiState
-import com.cairosquad.evolvefit.viewmodel.suggested_meals.SuggestedMealsScreenState
-import com.cairosquad.evolvefit.viewmodel.suggested_meals.toSuggestedMealUiState
-import kotlinx.coroutines.delay
-import kotlinx.coroutines.launch
 
 class MealDetailsViewModel(
      private val manageNutritionUseCase: ManageNutritionUseCase
