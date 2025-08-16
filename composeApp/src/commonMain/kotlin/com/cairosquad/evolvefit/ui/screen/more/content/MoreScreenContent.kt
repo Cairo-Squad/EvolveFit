6ef1254747af6beea88639b97466fe0f99eb44e8
Hager Abobakr <hagarabobakr1@gmail.com>
2025-08-16 11:49:39 +0300

    Refactor MoreScreenContent UI

--- composeApp/src/commonMain/kotlin/com/cairosquad/evolvefit/ui/screen/more/content/MoreScreenContent.kt
+++ composeApp/src/commonMain/kotlin/com/cairosquad/evolvefit/ui/screen/more/content/MoreScreenContent.kt
@@ -63,7 +63,8 @@ fun MoreScreenContent(
     listener: MoreInteractionListener
 ) {
     Column(
-        modifier = Modifier.fillMaxSize().background(color = Theme.color.surfaces.surface)
+        modifier = Modifier.fillMaxSize()
+            .background(color = Theme.color.surfaces.surface)
             .systemBarsPadding().padding(top = 40.dp),
         horizontalAlignment = Alignment.CenterHorizontally,
     ) {
@@ -165,8 +166,8 @@ fun PersonInfo(
         modifier = modifier
             .fillMaxWidth()
             .padding(horizontal = 16.dp)
-            .background(color = Theme.color.surfaces.surfaceContainer)
             .clip(RoundedCornerShape(8.dp))
+            .background(color = Theme.color.surfaces.surfaceContainer)
             .padding(vertical = 12.dp, horizontal = 16.dp),
         verticalAlignment = Alignment.CenterVertically,
         horizontalArrangement = Arrangement.spacedBy(12.dp)
