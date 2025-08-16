ddbcfaeb9dd1812fe4b677969641f2eae20736fc
Elsayed Magdy <saikoo1239@gmail.com>
2025-08-16 14:24:54 +0300

    refactor : cancel ripple

--- composeApp/src/commonMain/kotlin/com/cairosquad/evolvefit/design_system/component/PrimaryButton.kt
+++ composeApp/src/commonMain/kotlin/com/cairosquad/evolvefit/design_system/component/PrimaryButton.kt
@@ -3,7 +3,6 @@ package com.cairosquad.evolvefit.design_system.component
 import androidx.compose.animation.animateColorAsState
 import androidx.compose.foundation.background
 import androidx.compose.foundation.clickable
-import androidx.compose.foundation.interaction.MutableInteractionSource
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
@@ -11,7 +10,6 @@ import androidx.compose.foundation.shape.CircleShape
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.getValue
-import androidx.compose.runtime.remember
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.clip
@@ -49,14 +47,7 @@ fun PrimaryButton(
             .fillMaxWidth()
             .clip(CircleShape)
             .background(containerColor)
-            .clickable(
-                interactionSource = remember { MutableInteractionSource() },
-                indication = null
-            ) {
-                if (isEnabled) {
-                    onClick()
-                }
-            },
+            .clickable(enabled = isEnabled, onClick = onClick),
         contentAlignment = Alignment.Center
     ) {
         Text(
