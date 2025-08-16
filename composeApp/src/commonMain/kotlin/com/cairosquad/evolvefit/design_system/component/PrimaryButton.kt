06eb5b5baf1218c1b77833ce22ad7cf9989a0ecc
Elsayed Magdy <saikoo1239@gmail.com>
2025-08-16 14:12:04 +0300

    refactor : remove ripple effect

--- composeApp/src/commonMain/kotlin/com/cairosquad/evolvefit/design_system/component/PrimaryButton.kt
+++ composeApp/src/commonMain/kotlin/com/cairosquad/evolvefit/design_system/component/PrimaryButton.kt
@@ -3,6 +3,7 @@ package com.cairosquad.evolvefit.design_system.component
 import androidx.compose.animation.animateColorAsState
 import androidx.compose.foundation.background
 import androidx.compose.foundation.clickable
+import androidx.compose.foundation.interaction.MutableInteractionSource
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
@@ -10,6 +11,7 @@ import androidx.compose.foundation.shape.CircleShape
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.getValue
+import androidx.compose.runtime.remember
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.clip
@@ -47,7 +49,14 @@ fun PrimaryButton(
             .fillMaxWidth()
             .clip(CircleShape)
             .background(containerColor)
-            .clickable(enabled = isEnabled, onClick = onClick),
+            .clickable(
+                interactionSource = remember { MutableInteractionSource() },
+                indication = null
+            ) {
+                if (isEnabled) {
+                    onClick()
+                }
+            },
         contentAlignment = Alignment.Center
     ) {
         Text(
