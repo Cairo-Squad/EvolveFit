ceea67f739069456b3b2aa67da5eff000e67506f
gehadepada <gehadepada@gmail.com>
2025-08-19 04:54:02 +0300

    refactor: add bottom sheets and navigation

--- composeApp/src/commonMain/kotlin/com/cairosquad/evolvefit/ui/navigation/NavigationHost.kt
+++ composeApp/src/commonMain/kotlin/com/cairosquad/evolvefit/ui/navigation/NavigationHost.kt
@@ -29,6 +29,7 @@ import com.cairosquad.evolvefit.ui.screen.playWorkout.PlayWorkoutScreen
 import com.cairosquad.evolvefit.ui.screen.register.RegisterScreen
 import com.cairosquad.evolvefit.ui.screen.suggestedMeals.SuggestedMealsScreen
 import com.cairosquad.evolvefit.ui.screen.createWorkout.CreateWorkoutScreen
+import com.cairosquad.evolvefit.ui.screen.editProfile.EditProfileScreen
 import com.cairosquad.evolvefit.ui.screen.workoutDetails.WorkoutDetailsScreen
 import com.cairosquad.evolvefit.ui.screen.workoutHistory.WorkoutHistoryScreen
 import org.koin.compose.koinInject
@@ -216,7 +217,11 @@ fun NavigationHost(
                 navigateBack = navController::popBackStack
             )
         }
-
+        composable<EditProfileRoute> {
+            EditProfileScreen(
+                navigateBack = navController::popBackStack
+            )
+        }
         composable<FavoritesScreenRoute> {
             FavoritesScreen(
                 navigateBack = navController::popBackStack
