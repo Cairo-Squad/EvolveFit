d44827cc62666f63e311e2f77946504942517bb0
Elsayed Magdy <saikoo1239@gmail.com>
2025-08-22 16:27:47 +0300

    feat: add FieldType enum and field-specific exceptions

--- composeApp/src/commonMain/kotlin/com/cairosquad/evolvefit/viewmodel/utils/handleUiErrors.kt
+++ composeApp/src/commonMain/kotlin/com/cairosquad/evolvefit/viewmodel/utils/handleUiErrors.kt
@@ -4,8 +4,10 @@ import com.cairosquad.evolvefit.domain.exception.ExceededCaloriesException
 import com.cairosquad.evolvefit.domain.exception.ExceededWaterLimitException
 import com.cairosquad.evolvefit.domain.exception.InternetConnectionException
 import com.cairosquad.evolvefit.domain.exception.InvalidNumberFormatException
+import com.cairosquad.evolvefit.domain.exception.LengthTooLargeException
 import com.cairosquad.evolvefit.domain.exception.MealNotFoundException
 import com.cairosquad.evolvefit.domain.exception.NetworkException
+import com.cairosquad.evolvefit.domain.exception.NumberTooLargeException
 import com.cairosquad.evolvefit.domain.exception.TimeoutException
 import evolvefit.composeapp.generated.resources.Res
 import evolvefit.composeapp.generated.resources.error_exceeded_calories
@@ -13,12 +15,16 @@ import evolvefit.composeapp.generated.resources.error_exceeded_water
 import evolvefit.composeapp.generated.resources.error_invalid_number
 import evolvefit.composeapp.generated.resources.error_meal_not_found
 import evolvefit.composeapp.generated.resources.error_no_internet
+import evolvefit.composeapp.generated.resources.error_number_too_large
+import evolvefit.composeapp.generated.resources.error_text_too_long
 import evolvefit.composeapp.generated.resources.error_unknown
 import org.jetbrains.compose.resources.StringResource
 
 fun Throwable.toErrorMessageRes(): StringResource {
     return when (this) {
         is InvalidNumberFormatException -> Res.string.error_invalid_number
+        is LengthTooLargeException -> Res.string.error_text_too_long
+        is NumberTooLargeException -> Res.string.error_number_too_large
         is ExceededCaloriesException -> Res.string.error_exceeded_calories
         is ExceededWaterLimitException -> Res.string.error_exceeded_water
         is MealNotFoundException -> Res.string.error_meal_not_found
