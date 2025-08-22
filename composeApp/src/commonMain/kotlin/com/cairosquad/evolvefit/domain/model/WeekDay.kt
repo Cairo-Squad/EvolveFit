880f059ceb58bef445ed1b5167938c8cb919b141
Youssef Mohamed <youssef.mu.saber@gmail.com>
2025-08-22 15:21:51 +0300

    refactor: fixed the graphs style and the issue with the water consumed.

--- composeApp/src/commonMain/kotlin/com/cairosquad/evolvefit/domain/model/WeekDay.kt
+++ composeApp/src/commonMain/kotlin/com/cairosquad/evolvefit/domain/model/WeekDay.kt
@@ -1,11 +1,11 @@
 package com.cairosquad.evolvefit.domain.model
 
 enum class WeekDay {
+    SATURDAY,
+    SUNDAY,
     MONDAY,
     TUESDAY,
     WEDNESDAY,
     THURSDAY,
     FRIDAY,
-    SATURDAY,
-    SUNDAY
 }
