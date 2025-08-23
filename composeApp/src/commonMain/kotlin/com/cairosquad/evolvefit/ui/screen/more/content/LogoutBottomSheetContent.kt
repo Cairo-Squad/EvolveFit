85b118c5c3a0333b268623047a54bf1cbd4e2550
Menna Srour <mennasrour441@gmail.com>
2025-08-23 13:47:46 +0300

    display logout bottom sheet icon using UiImageDisplayer to have correct colors

--- composeApp/src/commonMain/kotlin/com/cairosquad/evolvefit/ui/screen/more/content/LogoutBottomSheetContent.kt
+++ composeApp/src/commonMain/kotlin/com/cairosquad/evolvefit/ui/screen/more/content/LogoutBottomSheetContent.kt
@@ -4,7 +4,6 @@ import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
-import androidx.compose.material3.Icon
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
@@ -12,26 +11,27 @@ import androidx.compose.ui.Modifier
 import androidx.compose.ui.unit.dp
 import com.cairosquad.evolvefit.design_system.component.PrimaryButton
 import com.cairosquad.evolvefit.design_system.theme.Theme
+import com.cairosquad.evolvefit.ui.component.UiImageDisplayer
+import com.cairosquad.evolvefit.viewmodel.onboarding.models.UiImage
 import evolvefit.composeapp.generated.resources.Res
 import evolvefit.composeapp.generated.resources.confirm_logout
 import evolvefit.composeapp.generated.resources.icon_login
 import evolvefit.composeapp.generated.resources.logo
 import evolvefit.composeapp.generated.resources.logout
 import evolvefit.composeapp.generated.resources.logout_message
-import org.jetbrains.compose.resources.painterResource
 import org.jetbrains.compose.resources.stringResource
 
 
 @Composable
 fun LogoutBottomSheetContent(
-    onLogout: () -> Unit
+    onLogout: () -> Unit,
 ) {
     Column(
         modifier = Modifier.padding(horizontal = 16.dp).fillMaxWidth(),
         horizontalAlignment = Alignment.CenterHorizontally
     ) {
-        Icon(
-            painter = painterResource(Res.drawable.icon_login),
+        UiImageDisplayer(
+            image = UiImage.ImageResource(Res.drawable.icon_login),
             contentDescription = stringResource(Res.string.logo),
             modifier = Modifier.size(80.dp).padding(bottom = 16.dp)
         )
