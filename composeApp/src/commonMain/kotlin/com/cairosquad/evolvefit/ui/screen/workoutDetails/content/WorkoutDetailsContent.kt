1af127bbc239c93dfee918a5613d7f0152667bf1
Zyad Hammad <zyad.nasser@systemira.net>
2025-08-23 14:58:33 +0300

    fix: scrolling all the way in workout details

--- composeApp/src/commonMain/kotlin/com/cairosquad/evolvefit/ui/screen/workoutDetails/content/WorkoutDetailsContent.kt
+++ composeApp/src/commonMain/kotlin/com/cairosquad/evolvefit/ui/screen/workoutDetails/content/WorkoutDetailsContent.kt
@@ -132,7 +132,7 @@ fun WorkoutDetailsContent(
                 .fillMaxSize()
                 .background(color = Theme.color.surfaces.surface),
             horizontalAlignment = Alignment.CenterHorizontally,
-            verticalArrangement = Arrangement.spacedBy(12.dp)
+            verticalArrangement = Arrangement.spacedBy(12.dp),
         ) {
             item {
                 NetworkImage(
@@ -170,6 +170,7 @@ fun WorkoutDetailsContent(
                 Exercises(
                     modifier = Modifier
                         .padding(horizontal = 16.dp)
+                        .padding(bottom = 24.dp)
                         .fillMaxWidth(),
                     exercises = state.workout.exercises,
                     onExerciseClick = { listener.onExerciseClicked(it) }
