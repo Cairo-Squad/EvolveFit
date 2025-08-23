5902fad79d7b76d946973ec3e5a86026d63051ab
Al-Qassim <71926296+Al-Qassim@users.noreply.github.com>
2025-08-23 05:58:08 +0300

    fix icon tint, and app bar background color in workout details

--- composeApp/src/commonMain/kotlin/com/cairosquad/evolvefit/ui/screen/workoutDetails/content/ExerciseBottomSheetContent.kt
+++ composeApp/src/commonMain/kotlin/com/cairosquad/evolvefit/ui/screen/workoutDetails/content/ExerciseBottomSheetContent.kt
@@ -5,6 +5,7 @@ import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxHeight
 import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.heightIn
 import androidx.compose.foundation.layout.navigationBarsPadding
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.rememberScrollState
@@ -19,6 +20,7 @@ import com.cairosquad.evolvefit.design_system.component.PrimaryButton
 import com.cairosquad.evolvefit.design_system.theme.AppTheme
 import com.cairosquad.evolvefit.design_system.theme.Theme
 import com.cairosquad.evolvefit.ui.screen.workoutDetails.content.component.ImageCarousel
+import com.cairosquad.evolvefit.ui.util.ScreenSize
 import com.cairosquad.evolvefit.viewmodel.workout_details.WorkoutDetailsScreenState
 import evolvefit.composeapp.generated.resources.Res
 import evolvefit.composeapp.generated.resources.bullet_point
@@ -42,8 +44,7 @@ fun ExerciseBottomSheetContent(
 ) {
     Column(
         modifier = Modifier.padding(horizontal = 16.dp)
-            .fillMaxHeight(0.95f)
-            .padding(horizontal = 16.dp)
+            .heightIn(max = ScreenSize.heightDp.dp * 0.95f)
             .verticalScroll(rememberScrollState())
     ) {
         exercise?.let {
