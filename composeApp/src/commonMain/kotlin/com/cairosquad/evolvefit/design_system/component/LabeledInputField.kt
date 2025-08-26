61ceb1a8f2115740a1c3e26a5134d098ce1c3a4b
Hager Abobakr <hagarabobakr1@gmail.com>
2025-08-26 14:42:24 +0300

    fix ripple effect in labeled input field

--- composeApp/src/commonMain/kotlin/com/cairosquad/evolvefit/design_system/component/LabeledInputField.kt
+++ composeApp/src/commonMain/kotlin/com/cairosquad/evolvefit/design_system/component/LabeledInputField.kt
@@ -1,28 +1,20 @@
 package com.cairosquad.evolvefit.design_system.component
 
-import androidx.compose.animation.AnimatedContent
-import androidx.compose.animation.core.tween
-import androidx.compose.animation.scaleIn
-import androidx.compose.animation.scaleOut
-import androidx.compose.animation.togetherWith
 import androidx.compose.foundation.background
-import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.offset
 import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.HorizontalDivider
-import androidx.compose.material3.Icon
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.unit.dp
+import com.cairosquad.evolvefit.design_system.component.appbar.ActionIconButton
 import com.cairosquad.evolvefit.design_system.composables.InputField
 import com.cairosquad.evolvefit.design_system.theme.Theme
-import evolvefit.composeapp.generated.resources.Res
-import evolvefit.composeapp.generated.resources.ic_arrow_down
 import org.jetbrains.compose.resources.DrawableResource
 import org.jetbrains.compose.resources.painterResource
 
@@ -55,23 +47,14 @@ fun LabeledInputField(
                 modifier = Modifier.weight(1f)
             )
             if (trailingIcon != null) {
-                AnimatedContent(
-                    targetState = trailingIcon,
-                    transitionSpec = {
-                        scaleIn(animationSpec = tween(300)).togetherWith(
-                            scaleOut(animationSpec = tween(300))
-                        )
-                    }
-                ) {
-                    Icon(
-                        painter = painterResource(it),
-                        contentDescription = null,
-                        tint = Theme.color.surfaces.onSurfaceVariant,
-                        modifier = Modifier
-                            .padding(start = 8.dp)
-                            .clickable { onClick?.invoke() }
-                    )
-                }
+                ActionIconButton(
+                    modifier = Modifier
+                        .padding(start = 8.dp),
+                    icon = painterResource(trailingIcon),
+                    contentDescription = null,
+                    tint = Theme.color.surfaces.onSurfaceVariant,
+                    onClick = { onClick?.invoke() }
+                )
             }
         }
 
@@ -83,7 +66,7 @@ fun LabeledInputField(
             trailingIcon = null,
             onTrailingIconClick = onClick,
             onClick = onClick,
-            modifier = Modifier.fillMaxWidth().offset(x=(-10).dp),
+            modifier = Modifier.fillMaxWidth().offset(x = (-10).dp),
             verticalPadding = 8.dp
         )
 
