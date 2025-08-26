41d91ca53cf91555df03210be9d2c804aaacd424
Al-Qassim <71926296+Al-Qassim@users.noreply.github.com>
2025-08-26 14:48:32 +0300

    use correct name for HttpClientHolder in different platform

--- composeApp/src/androidMain/kotlin/com/cairosquad/evolvefit/repository/utils/HttpClientHolder.android.kt
+++ composeApp/src/androidMain/kotlin/com/cairosquad/evolvefit/repository/utils/HttpClientHolder.android.kt
@@ -3,7 +3,6 @@ package com.cairosquad.evolvefit.repository.utils
 import io.ktor.client.engine.HttpClientEngineConfig
 import io.ktor.client.engine.HttpClientEngineFactory
 import io.ktor.client.engine.android.Android
-import io.ktor.client.engine.android.AndroidEngineConfig
 
 actual val platformHttpClientEngineFactory: HttpClientEngineFactory<HttpClientEngineConfig>
     get() = Android
