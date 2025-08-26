916b369b23272dc46d9215bad0af3191fd7e264c
gehadepada <gehadepada@gmail.com>
2025-08-26 15:37:35 +0300

    fix:fix delete is loading parameter

--- composeApp/src/commonMain/kotlin/com/cairosquad/evolvefit/viewmodel/meal_details/MealDetailsScreenState.kt
+++ composeApp/src/commonMain/kotlin/com/cairosquad/evolvefit/viewmodel/meal_details/MealDetailsScreenState.kt
@@ -10,28 +10,29 @@ import org.jetbrains.compose.resources.StringResource
 data class MealDetailsScreenState(
     val mealDetails: MealDetailsUiState = MealDetailsUiState(),
     val errorMessage: String? = null,
-    val isLoading :Boolean =false,
     val screenStatus: ScreenStatus = ScreenStatus.LOADING,
-    val showSaveMealSuccessSnackBar : Boolean = false
-){
+    val showSaveMealSuccessSnackBar: Boolean = false
+) {
     data class MealDetailsUiState(
-        val name : String = "",
-        val imgUrl : String = "",
-        val description : String = "",
-        val mealType : MealTypeUiState = MealTypeUiState.Breakfast,
-        val calories : Int = 0,
-        val protein : Int = 0,
-        val carbs : Int = 0,
-        val fat : Int = 0,
-        val ingredients : List<String> = emptyList(),
-        val isFavouriteMeal : Boolean = false
+        val name: String = "",
+        val imgUrl: String = "",
+        val description: String = "",
+        val mealType: MealTypeUiState = MealTypeUiState.Breakfast,
+        val calories: Int = 0,
+        val protein: Int = 0,
+        val carbs: Int = 0,
+        val fat: Int = 0,
+        val ingredients: List<String> = emptyList(),
+        val isFavouriteMeal: Boolean = false
     )
+
     enum class MealTypeUiState(val displayName: StringResource) {
         Breakfast(Res.string.meal_type_breakfast),
         Lunch(Res.string.meal_type_lunch),
         Dinner(Res.string.meal_type_dinner),
         Snacks(Res.string.meal_type_snacks)
     }
+
     enum class ScreenStatus {
         LOADING,
         ERROR,
