f81b524a0b79ef58842b97c43f26b379a1c528e5
Elsayed Magdy <saikoo1239@gmail.com>
2025-08-27 21:02:27 +0300

    feat: enhance PlayedWorkoutDao and update PlayedWorkoutEntity

--- composeApp/src/commonMain/kotlin/com/cairosquad/evolvefit/repository/workout/local/entity/PlayedWorkoutEntity.kt
+++ composeApp/src/commonMain/kotlin/com/cairosquad/evolvefit/repository/workout/local/entity/PlayedWorkoutEntity.kt
@@ -5,7 +5,8 @@ import androidx.room.PrimaryKey
 
 @Entity(tableName = "played_workouts")
 data class PlayedWorkoutEntity(
-    @PrimaryKey(autoGenerate = true) val localId: Long = 0,
+    @PrimaryKey(autoGenerate = true)
+    val id: Long = 0,
     val workoutId: String,
     val durationSeconds: Int,
     val isSynced: Boolean = false
