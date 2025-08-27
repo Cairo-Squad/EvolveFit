7933740a3fcf56d503159722d7b546d82d10d77b
Elsayed Magdy <saikoo1239@gmail.com>
2025-08-27 15:12:25 +0300

    feat: implement local caching for played workouts

---
+++ composeApp/src/commonMain/kotlin/com/cairosquad/evolvefit/repository/workout/local/PlayedWorkoutDao.kt
@@ -0,0 +1,20 @@
+package com.cairosquad.evolvefit.repository.workout.local
+
+import androidx.room.Dao
+import androidx.room.Insert
+import androidx.room.Query
+import androidx.room.Update
+import com.cairosquad.evolvefit.repository.workout.local.entity.PlayedWorkoutEntity
+
+
+@Dao
+interface PlayedWorkoutDao {
+    @Insert
+    suspend fun insertPlayedWorkout(workout: PlayedWorkoutEntity)
+
+    @Query("SELECT * FROM played_workouts WHERE isSynced = 0")
+    suspend fun getPendingWorkouts(): List<PlayedWorkoutEntity>
+
+    @Update
+    suspend fun updatePlayedWorkouts(workouts: List<PlayedWorkoutEntity>)
+}
