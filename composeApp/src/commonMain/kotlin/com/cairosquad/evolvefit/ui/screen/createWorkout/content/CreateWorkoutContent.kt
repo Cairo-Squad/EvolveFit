f8802ec55e91391de0d4fec19e9405041c014089
Elsayed Magdy <saikoo1239@gmail.com>
2025-08-27 10:02:17 +0300

    fix: workout creation bottom button visibility

--- composeApp/src/commonMain/kotlin/com/cairosquad/evolvefit/ui/screen/createWorkout/content/CreateWorkoutContent.kt
+++ composeApp/src/commonMain/kotlin/com/cairosquad/evolvefit/ui/screen/createWorkout/content/CreateWorkoutContent.kt
@@ -8,6 +8,7 @@ import androidx.compose.foundation.layout.WindowInsets
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.navigationBars
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.statusBars
 import androidx.compose.foundation.layout.windowInsetsPadding
@@ -188,6 +189,7 @@ fun CreateWorkoutContent(
             isEnabled = state.isNextEnabled,
             modifier = Modifier
                 .fillMaxWidth()
+                .windowInsetsPadding(WindowInsets.navigationBars)
                 .padding(horizontal = 16.dp, vertical = 16.dp)
                 .padding(bottom = 24.dp)
         )
