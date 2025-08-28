440d4a9c9279f551095b2ca78b656e1fcd7ac69a
Elsayed Magdy <saikoo1239@gmail.com>
2025-08-28 13:52:09 +0300

    Refactor: WorkoutsScreenContent structure and add empty screen

--- composeApp/src/commonMain/kotlin/com/cairosquad/evolvefit/ui/screen/workout/content/WorkoutsScreenContent.kt
+++ composeApp/src/commonMain/kotlin/com/cairosquad/evolvefit/ui/screen/workout/content/WorkoutsScreenContent.kt
@@ -5,10 +5,12 @@ import androidx.compose.animation.core.FastOutSlowInEasing
 import androidx.compose.animation.core.tween
 import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.statusBarsPadding
+import androidx.compose.foundation.lazy.rememberLazyListState
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
@@ -16,142 +18,81 @@ import androidx.compose.ui.unit.dp
 import com.cairosquad.evolvefit.design_system.theme.Theme
 import com.cairosquad.evolvefit.ui.component.RefreshBox
 import com.cairosquad.evolvefit.ui.screen.workout.AddWorkoutBtn
+import com.cairosquad.evolvefit.ui.screen.workout.WorkoutsEmptyScreen
 import com.cairosquad.evolvefit.ui.screen.workout.content.compnent.WorkoutAppBar
 import com.cairosquad.evolvefit.viewmodel.workout.WorkoutInteractionListener
 import com.cairosquad.evolvefit.viewmodel.workout.WorkoutScreenState
 import org.jetbrains.compose.resources.stringResource
 
-//@Composable
-//fun WorkoutsScreenContent(
-//    state: WorkoutScreenState,
-//    listener: WorkoutInteractionListener
-//) {
-//    RefreshBox(
-//        isRefreshing = state.isRefreshing,
-//        onRefresh = listener::onRefresh
-//    ) {
-//        Column(
-//            modifier = Modifier
-//                .fillMaxSize()
-//                .background(color = Theme.color.surfaces.surface)
-//                .statusBarsPadding(),
-//            horizontalAlignment = Alignment.CenterHorizontally,
-//            verticalArrangement = Arrangement.spacedBy(12.dp)
-//        ) {
-//
-//            WorkoutAppBar(listener::onCommunityClicked)
-//
-//            WorkoutFocusAreaFilter(
-//                focusArea = WorkoutScreenState.FocusAreaUiState.entries,
-//                selectedFocusArea = state.selectedFocusArea,
-//                onSelectFocusArea = listener::onFocusAreaSelected
-//            )
-//
-//            Crossfade(
-//                targetState = state.screenStatus,
-//                animationSpec = tween(
-//                    durationMillis = 400,
-//                    easing = FastOutSlowInEasing
-//                )
-//            ) { status ->
-//                when (status) {
-//                    WorkoutScreenState.ScreenStatus.SUCCESS -> {
-//                        Workouts(
-//                            workouts = state.allWorkouts,
-//                            selected = state.selectedFocusArea,
-//                            onClickWorkout = listener::onWorkoutClicked
-//                        )
-//                    }
-//
-//                    WorkoutScreenState.ScreenStatus.LOADING -> {
-//                        WorkoutsLoadingScreen()
-//                    }
-//
-//                    WorkoutScreenState.ScreenStatus.FAIL -> {
-//                        WorkoutsErrorScreen(
-//                            message = state.errorMessage?.let { stringResource(it) },
-//                            onRetry = listener::onRetryClicked
-//                        )
-//                    }
-//
-//                    WorkoutScreenState.ScreenStatus.EMPTY -> {
-//
-//                    }
-//                }
-//            }
-//        }
-//        if (state.screenStatus == WorkoutScreenState.ScreenStatus.SUCCESS) {
-//            AddWorkoutBtn(
-//                modifier = Modifier.padding(24.dp).align(Alignment.BottomEnd),
-//                listener::onAddWorkoutClicked
-//            )
-//        }
-//    }
-//}
 @Composable
 fun WorkoutsScreenContent(
     state: WorkoutScreenState,
     listener: WorkoutInteractionListener
 ) {
-    RefreshBox(
-        isRefreshing = state.isRefreshing,
-        onRefresh = listener::onRefresh
+    val listState = rememberLazyListState()
+    Box(
+        modifier = Modifier
+            .fillMaxSize()
+            .background(Theme.color.surfaces.surface)
     ) {
-        Column(
-            modifier = Modifier
-                .fillMaxSize()
-                .background(color = Theme.color.surfaces.surface)
-                .statusBarsPadding(),
-            horizontalAlignment = Alignment.CenterHorizontally,
-            verticalArrangement = Arrangement.spacedBy(12.dp)
+        RefreshBox(
+            modifier = Modifier.fillMaxSize(),
+            isRefreshing = state.isRefreshing,
+            onRefresh = listener::onRefresh
         ) {
+            Column(
+                modifier = Modifier
+                    .fillMaxSize()
+                    .statusBarsPadding(),
+                horizontalAlignment = Alignment.CenterHorizontally,
+                verticalArrangement = Arrangement.spacedBy(12.dp)
+            ) {
 
-            WorkoutAppBar(listener::onCommunityClicked)
+                WorkoutAppBar(listener::onCommunityClicked)
 
-            Crossfade(
-                targetState = state.screenStatus,
-                animationSpec = tween(
-                    durationMillis = 400,
-                    easing = FastOutSlowInEasing
-                )
-            ) { status ->
-                when (status) {
-                    WorkoutScreenState.ScreenStatus.SUCCESS -> {
-                        Column {
-                            WorkoutFocusAreaFilter(
-                                focusArea = WorkoutScreenState.FocusAreaUiState.entries,
-                                selectedFocusArea = state.selectedFocusArea,
-                                onSelectFocusArea = listener::onFocusAreaSelected
-                            )
-                            Workouts(
-                                workouts = state.allWorkouts,
-                                selected = state.selectedFocusArea,
-                                onClickWorkout = listener::onWorkoutClicked
+                Crossfade(
+                    targetState = state.screenStatus,
+                    animationSpec = tween(
+                        durationMillis = 400,
+                        easing = FastOutSlowInEasing
+                    )
+                ) { status ->
+                    when (status) {
+                        WorkoutScreenState.ScreenStatus.SUCCESS -> {
+                            WorkoutsSuccessScreen(
+                                lazyListState = listState,
+                                state = state,
+                                listener = listener
                             )
                         }
-                    }
 
-                    WorkoutScreenState.ScreenStatus.LOADING -> {
-                        WorkoutsLoadingScreen() // جواها CategoriesShimmerRow
-                    }
+                        WorkoutScreenState.ScreenStatus.LOADING -> {
+                            WorkoutsLoadingScreen(isItemsLoading = state.isItemsLoading)
+                        }
 
-                    WorkoutScreenState.ScreenStatus.FAIL -> {
-                        WorkoutsErrorScreen(
-                            message = state.errorMessage?.let { stringResource(it) },
-                            onRetry = listener::onRetryClicked
-                        )
-                    }
+                        WorkoutScreenState.ScreenStatus.FAIL -> {
+                            WorkoutsErrorScreen(
+                                message = state.errorMessage?.let { stringResource(it) },
+                                onRetry = listener::onRetryClicked
+                            )
+                        }
 
-                    WorkoutScreenState.ScreenStatus.EMPTY -> {
-                        // ممكن تضيف شاشة فارغة هنا
+                        WorkoutScreenState.ScreenStatus.EMPTY -> {
+                            WorkoutsEmptyScreen(
+                                lazyListState = listState,
+                                state = state,
+                                listener = listener
+                            )
+                        }
                     }
                 }
             }
         }
-
-        if (state.screenStatus == WorkoutScreenState.ScreenStatus.SUCCESS) {
+        if (state.screenStatus != WorkoutScreenState.ScreenStatus.FAIL) {
             AddWorkoutBtn(
-                modifier = Modifier.padding(24.dp).align(Alignment.BottomEnd),
+                modifier = Modifier
+                    .padding(24.dp)
+                    .align(Alignment.BottomEnd),
                 listener::onAddWorkoutClicked
             )
         }
